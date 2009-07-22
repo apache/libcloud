80e082a2dd91506f5c0d3c31da6eca0212054cfc
Alex Polvi <polvi@apache.org>
2009-07-22 23:49:06 +0000

    updates to the example and readme
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@886370 13f79535-47bb-0310-9956-ffa450edef68

--- example.py
+++ example.py
@@ -1,7 +1,14 @@
 from libcloud.providers import connect
 from libcloud.types import Provider
 
-conn = connect(Provider.EC2, 'access key id', 'secret key')
-print conn.list_nodes()
-conn = connect(Provider.DUMMY, 'blah')
-print conn.list_nodes()
+ec2 = connect(Provider.EC2, 'access key id', 'secret key')
+slicehost = connect(Provider.SLICEHOST, 'api key')
+rackspace = connect(Provider.RACKSPACE, 'username', 'api key')
+
+all_servers = []
+for provider in [ ec2, slicehost, rackspace ]:
+  all_servers.extend(provider.list_nodes())
+
+# all_servers now has a list of node objects 
+# from ec2, slicehost, and rackspace
+print all_servers
