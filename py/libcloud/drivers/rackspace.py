da3a76ae4ebaddc8d6cb303c6287f68ed5d4e21c
Alex Polvi <polvi@apache.org>
2009-07-26 23:56:15 +0000

    made rackspace impl throw InvalidCredsException, playing with it
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@886417 13f79535-47bb-0310-9956-ffa450edef68

--- py/libcloud/drivers/rackspace.py
+++ py/libcloud/drivers/rackspace.py
@@ -12,22 +12,34 @@ NAMESPACE = 'http://docs.rackspacecloud.com/servers/api/v1.0'
 
 class RackspaceConnection(object):
   def __init__(self, user, key):
-
     self.user = user
     self.key = key
+    self.token = None
+    self.endpoint = None
+
+  def _authenticate(self):
     self.auth = httplib.HTTPSConnection("%s:%d" % (AUTH_HOST, 443))
-    self.auth.request('GET', '/%s' % API_VERSION, headers={'X-Auth-User': user, 'X-Auth-Key': key})
+    self.auth.request('GET', '/%s' % API_VERSION, headers={'X-Auth-User': self.user, 'X-Auth-Key': self.key})
     ret = self.auth.getresponse()
     self.token = ret.getheader('x-auth-token')
     self.endpoint = ret.getheader('x-server-management-url')
+
+    if not self.token or not self.endpoint:
+      raise InvalidCredsException()
 
     (scheme, server, self.path, param, query, fragment) = urlparse.urlparse(self.endpoint)
     self.api = httplib.HTTPSConnection("%s:%d" % (server, 443))
 
   def _headers(self):
+    if not self.token:
+      self._authenticate()
+
     return {'X-Auth-Token': self.token, 'Accept': 'application/xml' }
 
   def make_request(self, path, data=''):
+    if not self.token or not self.endpoint:
+      self._authenticate()
+
     self.api.request('GET', '%s/%s' % (self.path, path), headers=self._headers())
     return self.api.getresponse()
 
