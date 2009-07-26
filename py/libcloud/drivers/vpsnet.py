65a3520990c29ffb2d52f718d47d401b0ac4f51d
Alex Polvi <polvi@apache.org>
2009-07-26 21:50:10 +0000

    move src to py, and put a teaser for the ruby kids
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@886413 13f79535-47bb-0310-9956-ffa450edef68

---
+++ py/libcloud/drivers/vpsnet.py
@@ -0,0 +1,72 @@
+from libcloud.types import NodeState, Node
+import base64
+import httplib
+import hashlib
+from xml.etree import ElementTree as ET
+
+API_HOST = 'vps.net'
+
+class VPSNetConnection(object):
+  def __init__(self, user, key):
+
+    self.user = user
+    self.key = key
+
+    self.api = httplib.HTTPSConnection("%s:%d" % (API_HOST, 443))
+
+  def _headers(self):
+    return {'Authorization': 'Basic %s' % (base64.b64encode('%s:%s' % (self.user, self.key))),
+            'Host': API_HOST }
+
+  def make_request(self, path, data=''):
+    self.api.request('GET', '%s' % (path), headers=self._headers())
+    return self.api.getresponse()
+
+  def virtual_machines(self):
+    return Response(self.make_request('/virtual_machines.xml'))
+
+class Response(object):
+  def __init__(self, http_response):
+    self.http_response = http_response
+    self.http_xml = http_response.read()
+
+class VPSNetProvider(object):
+
+  def __init__(self, creds):
+    self.creds = creds
+    self.api = VPSNetConnection(creds.key, creds.secret)
+
+  def _to_node(self, element):
+    attrs = [ 'backups_enabled', 'cloud_id', 'consumer_id', 'created_at',
+              'domain_name', 'hostname', 'id', 'label', 'password', 'system_template_id',
+              'updated_at', 'running', 'power_action_pending', 'slices_count' ]
+
+    node_attrs = {}
+    for attr in attrs:
+      node_attrs[attr] = element.findtext(attr)
+
+    ipaddress = element.findtext('ip-address')
+
+    running = element.findtext('running') == 'true'
+    power_action_pending = element.findtext('power_action_pending') == 'true'
+
+    state = NodeState.UNKNOWN
+    if power_action_pending:
+      state = NodeState.PENDING
+    elif running:
+      state = NodeState.RUNNING
+
+    n = Node(uuid = self.get_uuid(element.findtext('id')),
+             name = element.findtext('label'),
+             state = state,
+             ipaddress = None, #XXX:  they do not return this in the vps list!
+             creds = self.creds,
+             attrs = node_attrs)
+    return n
+
+  def get_uuid(self, field):
+    return hashlib.sha1("%s:%d" % (field,self.creds.provider)).hexdigest()
+
+  def list_nodes(self):
+    res = self.api.virtual_machines()
+    return [ self._to_node(el) for el in ET.XML(res.http_xml).findall('virtual_machine') ]
