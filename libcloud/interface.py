c359e88bf41fb7406d70ea3c82f41a7f7eb963c5
Tom Davis <tom@apache.org>
2009-07-27 06:40:16 +0000

    Initial IDriver interface [TODO]
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@886455 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/interface.py
+++ libcloud/interface.py
@@ -1,4 +1,31 @@
-from zope.interface import Interface
+from zope.interface import Interface, Attribute
+
+
+class IDriver(Interface):
+    """
+    TODO
+    
+    A Driver represents an interface to a provider Web Service. This is the
+    Interface which all base drivers (`EC2Driver`, `SliceDriver`, etc.) should
+    implement!
+    """
+    connectionCls = Attribute("""A suitable provider-specific Connection class""")
+    nodesCls = Attribute("""A suitable provider-specific NodeDriver class""")
+    connection = Attribute("""A suitable provider-specific Connection instance""")
+    nodes = Attribute("""A suitable provider-specific NodeDriver instance""")
+
+
+class IDriverFactory(Interface):
+    """
+    TODO
+
+    Creates IDrivers
+    """
+    def __call__():
+        """
+        Initialize `connection` and `nodes` 
+        """
+
 
 class INodeDriver(Interface):
   """
