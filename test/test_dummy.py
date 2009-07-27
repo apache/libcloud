ed6d01790cffa05a4812559a09bc0f6e9c4bcc7b
Tom Davis <tom@apache.org>
2009-07-27 06:38:18 +0000

    Update path for test initialization [closes #3]
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@886454 13f79535-47bb-0310-9956-ffa450edef68

--- test/test_dummy.py
+++ test/test_dummy.py
@@ -1,17 +1,3 @@
-# Licensed to libcloud.org under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# libcloud.org licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 import unittest
 
 from libcloud.providers import connect
@@ -19,16 +5,16 @@ from libcloud.types import Provider, Node
 
 class DummyTests(unittest.TestCase):
 
-    def setUp(self):
-        self.conn = connect(Provider.DUMMY, 'foo')
+  def setUp(self):
+    self.conn = connect(Provider.DUMMY, 'foo')
 
-    def test_list_nodes(self):
-        ret = self.conn.list_nodes()
+  def test_list_nodes(self):
+    ret = self.conn.list_nodes()
 
-    def test_reboot_node(self):
-        node = Node(None, None, None, None, None, attrs={})
-        ret = self.conn.reboot_node(node)
+  def test_reboot_node(self):
+    node = Node(None, None, None, None, None, attrs={})
+    ret = self.conn.reboot_node(node)
 
-    def test_create_node(self):
-        node = Node(None, None, None, None, None, attrs={})
-        ret = self.conn.create_node(node)
+  def test_create_node(self):
+    node = Node(None, None, None, None, None, attrs={})
+    ret = self.conn.create_node(node)
