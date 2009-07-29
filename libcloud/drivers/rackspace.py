b4646a34d38aee0863aeba942b014fac8703297c
jcsalterego <dev-null@apache.org>
2009-07-29 07:25:03 +0000

    Rackspace: Formatting tweaks
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@886441 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/drivers/rackspace.py
+++ libcloud/drivers/rackspace.py
@@ -20,8 +20,8 @@ class RackspaceConnection(object):
     def _authenticate(self):
         self.auth = httplib.HTTPSConnection("%s:%d" % (AUTH_HOST, 443))
         self.auth.request('GET', '/%s' % API_VERSION,
-                          headers={'X-Auth-User': self.user,
-                          'X-Auth-Key': self.key})
+                          headers={ 'X-Auth-User': self.user,
+                                    'X-Auth-Key': self.key })
         ret = self.auth.getresponse()
         self.token = ret.getheader('x-auth-token')
         self.endpoint = ret.getheader('x-server-management-url')
@@ -29,22 +29,24 @@ class RackspaceConnection(object):
         if not self.token or not self.endpoint:
             raise InvalidCredsException()
 
-        (scheme, server, self.path,
-         param, query, fragment) = urlparse.urlparse(self.endpoint)
+        scheme, server, self.path, param, query, fragment = (
+            urlparse.urlparse(self.endpoint)
+        )
         self.api = httplib.HTTPSConnection("%s:%d" % (server, 443))
 
     def _headers(self):
         if not self.token:
             self._authenticate()
 
-        return {'X-Auth-Token': self.token, 'Accept': 'application/xml' }
+        return { 'X-Auth-Token': self.token,
+                 'Accept': 'application/xml' }
 
     def make_request(self, path, data=''):
         if not self.token or not self.endpoint:
             self._authenticate()
 
         self.api.request('GET', '%s/%s' % (self.path, path),
-                                         headers=self._headers())
+                         headers=self._headers())
         return self.api.getresponse()
 
     def list_servers(self):
@@ -71,18 +73,17 @@ class RackspaceNodeDriver(object):
         return element.findtext(self._fixxpath(xpath))
 
     def _to_node(self, element):
-        states = {'BUILD':NodeState.PENDING,
-                  'ACTIVE':NodeState.RUNNING,
-                  'SUSPENDED':NodeState.TERMINATED,
-                  'QUEUE_RESIZE':NodeState.PENDING,
-                  'PREP_RESIZE':NodeState.PENDING,
-                  'RESCUE':NodeState.PENDING,
-                  'REBUILD':NodeState.PENDING,
-                  'REBOOT':NodeState.REBOOTING,
-                  'HARD_REBOOT':NodeState.REBOOTING }
+        states = { 'BUILD': NodeState.PENDING,
+                   'ACTIVE': NodeState.RUNNING,
+                   'SUSPENDED': NodeState.TERMINATED,
+                   'QUEUE_RESIZE': NodeState.PENDING,
+                   'PREP_RESIZE': NodeState.PENDING,
+                   'RESCUE': NodeState.PENDING,
+                   'REBUILD': NodeState.PENDING,
+                   'REBOOT': NodeState.REBOOTING,
+                   'HARD_REBOOT': NodeState.REBOOTING }
 
         attribs = element.attrib
-
         node_attrs = attribs
 
         try:
