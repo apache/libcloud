5b243e855d7e581e92daf436891eefd2f2834a62
Alex Polvi <polvi@apache.org>
2009-08-02 20:10:33 +0000

    Merge branch 'master' of git://github.com/tdavis/libcloud into tdavis
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@886474 13f79535-47bb-0310-9956-ffa450edef68

--- test/__init__.py
+++ test/__init__.py
@@ -12,3 +12,101 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import httplib
+from cStringIO import StringIO
+from urllib2 import urlparse
+
+
+class MockResponse(object):
+    """
+    A mock HTTPResponse
+    """
+    headers = {}
+    body = StringIO()
+    status = 0
+    reason = ''
+    version = 11
+
+    def __init__(self, status, body, headers=None, reason=None):
+        self.status = status
+        self.body = StringIO(body)
+        self.headers = headers or self.headers
+        self.reason = reason or self.reason
+
+    def read(self, *args, **kwargs):
+        return self.body.read(*args, **kwargs)
+    
+    def getheader(self, name, *args, **kwargs):
+        return self.headers.get(name, *args, **kwargs)
+    
+    def getheaders(self):
+        return self.headers.items()
+
+    def msg(self):
+        raise NotImplemented
+
+
+class MockHttp(object):
+    """
+    A mock HTTP client/server suitable for testing purposes. This replaces 
+    `HTTPConnection` by implementing its API and returning a mock response.
+
+    Define methods by request path, replacing slashes (/) with underscores (_).
+    Each of these mock methods should return a tuple of:
+        
+        (int status, str body, dict headers, str reason)
+
+    >>> mock = MockHttp('localhost', 8080)
+    >>> mock.request('GET', '/example/')
+    >>> response = mock.getresponse()
+    >>> response.body.read()
+    'Hello World!'
+    >>> response.status
+    200
+    >>> response.getheaders()
+    [('X-Foo', 'libcloud')]
+
+    """
+    responseCls = MockResponse
+    host = None
+    port = None
+    response = None
+
+    def __init__(self, host, port, *args, **kwargs):
+        self.host = host
+        self.port = port
+
+    def request(self, method, url, body=None, headers=None):
+        # Find a method we can use for this request
+        path = urlparse.urlparse(url)[2]
+        if path.endswith('/'):
+            path = path[:-1]
+        meth = getattr(self, path.replace('/','_'))
+        status, body, headers, reason = meth(method, url, body, headers)
+        self.response = self.responseCls(status, body, headers, reason)
+
+    def getresponse(self):
+        return self.response
+
+    def connect(self):
+        """
+        Can't think of anything to mock here.
+        """
+        pass
+
+    def close(self):
+        pass
+
+    # Mock request/response example
+    def _example(self, method, url, body, headers):
+        """
+        Return a simple message and header, regardless of input.
+        """
+        return (httplib.OK, 'Hello World!', {'X-Foo': 'libcloud'},
+                httplib.responses[httplib.OK])
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
