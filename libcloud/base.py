a28265f380e42f1a63f124ce59801f96eb3e0b19
Tom Davis <tom@apache.org>
2009-08-02 06:29:13 +0000

    Added some base classes to derive from [untested]
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@886469 13f79535-47bb-0310-9956-ffa450edef68

---
+++ libcloud/base.py
@@ -0,0 +1,162 @@
+import httplib, urllib
+from zope import interface
+from libcloud.interface import IConnectionUserAndKey, IResponse
+from libcloud.interface import IConnectionUserAndKeyFactory, IResponseFactory
+
+
+class Response(object):
+    """
+    A Base Response class to derive from.
+    """
+    interface.implements(IResponse)
+    interface.classProvides(IResponseFactory)
+
+    NODE_STATE_MAP = {}
+
+    tree = None
+    body = None
+    status_code = 200
+    headers = {}
+    error = None
+
+    def __init__(response):
+        self.body = response.read()
+        self.status = response.status
+        self.headers = dict(response.getheaders())
+        self.error = response.reason
+        self.tree = self.parse_body(self.body)
+
+    def parse_body(self, body):
+        """
+        Parse response body.
+
+        Override in a provider's subclass.
+
+        @type body: C{unicode}
+        @param body: Response body.
+        @return: Parsed body.
+        """
+        return body
+
+    def success(self):
+        """
+        Determine if our request was successful.
+
+        The meaning of this can be arbitrary; did we receive OK status? Did
+        the node get created? Were we authenticated?
+
+        @return: C{True} or C{False}
+        """
+        return self.status == httplib.OK
+
+    def to_node(self):
+        """
+        A method that knows how to convert a given response tree to a L{Node}.
+
+        Override in a provider's subclass.
+        """
+        raise NotImplemented
+
+
+class ConnectionKey(object):
+    """
+    A Base Connection class to derive from.
+    """
+    interface.implements(IConnectionUserAndKey)
+    interface.classProvides(IConnectionUserAndKeyFactory)
+
+    conn_classes = (httplib.HTTPConnection, httplib.HTTPSConnection)
+    responseCls = Response
+    connection = None
+    host = '127.0.0.1'
+    port = (8080, 8080)
+    secure = 1
+
+    def __init__(self, key, secure=True):
+        """
+        Initialize `user_id` and `key`; set `secure` to an C{int} based on
+        passed value.
+        """
+        self.key = key
+        self.secure = secure and 1 or 0
+
+    def connect(host=None, port=None):
+        """
+        Establish a connection with the API server.
+
+        @type host: C{str}
+        @param host: Optional host to override our default
+
+        @type port: C{int}
+        @param port: Optional port to override our default
+
+        @returns: A connection
+        """
+        host = host or self.host
+        port = port or self.port[self.secure]
+        proto = self.secure and 'https://' or 'http://'
+        uri = proto + host
+
+        connection = self.conn_classes[self.secure](uri, port)
+        self.connection = connection
+
+    def request(action, params={}, data='', method='GET'):
+        """
+        Request a given `action`.
+        
+        Basically a wrapper around the connection
+        object's `request` that does some helpful pre-processing.
+
+        @type action: C{str}
+        @param action: A path
+
+        @type params C{dict}
+        @param params: Optional mapping of additional parameters to send. If
+            None, leave as an empty C{dict}.
+
+        @type data: C{unicode}
+        @param data: A body of data to send with the request.
+
+        @type method: C{str}
+        @param method: An HTTP method such as "GET" or "POST".
+
+        @return: An instance of type I{responseCls}
+        """
+        # Extend default parameters
+        params = params.extend(self.default_params)
+        # Encode data if necessary
+        if data != '':
+            data = self.__encode_data(data)
+        url = '?'.join((action, urllib.urlencode(params)))
+        self.connection.request(method=method, url=url, body=data,
+                                headers=headers)
+        return self.responseCls(self.connection.getresponse())
+
+    @property
+    def default_params(self):
+        """
+        Return a dictionary of default parameters to add to query parameters.
+
+        Override in a provider's subclass.
+        """
+        return {}
+
+    def __encode_data(self, data):
+        """
+        Encode body data.
+
+        Override in a provider's subclass.
+        """
+        return data
+
+
+class ConnectionUserAndKey(ConnectionKey):
+    """
+    Base connection which accepts a user_id and key
+    """
+    user_id = None
+
+    def __init__(self, user_id, key, secure=True):
+        super(ConnectionUserAndKey, self).__init__(key, secure)
+        self.user_id = user_id
+
