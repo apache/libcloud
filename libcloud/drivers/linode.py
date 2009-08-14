1b98cdf395ecd6f46a206851cddbc0ef74d4a572
Jedidiah Smith <jed@apache.org>
2009-08-14 19:29:55 +0000

    Rework licensing issue
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@886538 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/drivers/linode.py
+++ libcloud/drivers/linode.py
@@ -1,19 +1,12 @@
 #
-# libcloud Linode Support
-# Copyright (C) 2009 Linode, LLC.  Licensed to libcloud.org; see LICENSE.MIT.
+# libcloud
+# A Unified Interface into The Cloud
+#
+# Linode Driver
+# Copyright (C) 2009 libcloud.org and contributors.
+# Released under license; see LICENSE for more information.
+#.
 # Maintainer: Jed Smith <jsmith@linode.com>
-#
-# libcloud.org licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 #
 # BETA TESTING THE LINODE API AND DRIVERS
 #
