70d8aac8781d85a2f0875d2fb885d8f1d2a6fc48
Jedidiah Smith <jed@apache.org>
2009-08-15 04:59:57 +0000

    Rough stab at create_node()
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@886540 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/drivers/linode.py
+++ libcloud/drivers/linode.py
@@ -113,6 +113,10 @@ class LinodeNodeDriver(NodeDriver):
     type = Provider.LINODE
     name = "Linode"
     connectionCls = LinodeConnection
+    
+    def __init__(self, key):
+        self.datacenter = None
+        NodeDriver.__init__(self, key)
 
     # Converts Linode's state from DB to a NodeState constant.
     # Some of these are lightly questionable.
@@ -145,7 +149,192 @@ class LinodeNodeDriver(NodeDriver):
         params = { "api_action": "linode.delete", "LinodeID": node.id,
             "skipChecks": True }
         self.connection.request(LINODE_ROOT, params=params)
-    
+
+    def create_node(self, name, image, size, **kwargs):
+        # Create
+        #
+        # Creates a Linode instance.
+        #
+        #       name     Used for a lot of things; be cautious with charset
+        #       image    NodeImage from list_images
+        #       size     NodeSize from list_sizes
+        #
+        # Keyword arguments supported:
+        #
+        #    One of the following is REQUIRED, but both can be given:
+        #       ssh      The SSH key to deploy for root (None).
+        #       root     Password to set for root (Random).
+        #
+        #    These are all optional:
+        #       swap     Size of the swap partition in MB (128).
+        #       rsize    Size of the root partition (plan size - swap).
+        #       kernel   A kernel ID from avail.kernels (Latest 2.6).
+        #       comment  Comments to store with the config (None).
+        #       payment  One of 1, 12, or 24; subscription length (1).
+        #
+        #    Labels to override what's generated (default on right):
+        #       lconfig      [%name] Instance
+        #       lrecovery    [%name] Finnix Recovery Configuration
+        #       lroot        [%name] %distro
+        #       lswap        [%name] Swap Space
+        #
+        # Datacenter logic:
+        #
+        #   As Linode requires choosing a datacenter, a little logic is done.
+        #
+        #   1. If the API key in use has all its Linodes in one DC, that DC will
+        #      be chosen (and can be overridden with linode_set_datacenter).
+        #
+        #   2. Otherwise (for both the "No Linodes" and "different DC" cases), a
+        #      datacenter must explicitly be chosen using linode_set_datacenter.
+        #
+        # Please note that for safety, only 5 Linodes can be created per hour.
+
+        # Step -1: Do the datacenter logic
+        fail = LinodeException(0xFC,
+            "Can't pick DC; choose a datacenter with linode_set_datacenter()")
+        if not self.datacenter:
+            # Okay, one has not been chosen.  We need to determine.
+            nodes = self.list_nodes()
+            num = len(nodes)
+            if num == 0:
+                # Won't assume where to deploy the first one.
+                # FIXME: Maybe we should?
+                raise fail
+            else:
+                # One or more nodes, so create the next one there.
+                chosen = nodes[0].extra["DATACENTERID"]
+                for node in nodes[1:]:
+                    # Check to make sure they're all the same
+                    if chosen != node.extra["DATACENTERID"]:
+                        raise fail
+        else:
+            # linode_set_datacenter() was used, cool.
+            chosen = self.datacenter
+
+        # Step 0: Parameter validation before we purchase
+        # We're especially careful here so we don't fail after purchase, rather
+        # than getting halfway through the process and having the API fail.
+
+        # Plan ID
+        plans = self.list_sizes()
+        if size.id not in [p.id for p in plans]:
+            raise LinodeException(0xFB, "Invalid plan ID -- avail.plans")
+
+        # Payment schedule
+        payment = "1" if "payment" not in kwargs else str(kwargs["payment"])
+        if payment not in ["1", "12", "24"]:
+            raise LinodeException(0xFB, "Invalid subscription (1, 12, 24)")
+
+        # SSH key and/or root password
+        ssh = None if "ssh" not in kwargs else kwargs["ssh"]
+        root = None if "root" not in kwargs else kwargs["root"]
+        if not ssh and not root:
+            raise LinodeException(0xFB, "Need SSH key or root password")
+
+        # Swap size
+        try: swap = 128 if "swap" not in kwargs else int(kwargs["swap"])
+        except: raise LinodeException(0xFB, "Need an integer swap size")
+
+        # Root partition size
+        imagesize = (size.disk - swap) if "rsize" not in kwargs else \
+            int(kwargs["rsize"])
+        if (imagesize + swap) > size.disk:
+            raise LinodeException(0xFB, "Total disk images are too big")
+
+        # Distribution ID
+        distros = self.list_images()
+        if image.id not in [d.id for d in distros]:
+            raise LinodeException(0xFB, "Invalid distro -- avail.distributions")
+
+        # Kernel
+        kernel = 60 if "kernel" not in kwargs else kwargs["kernel"]
+        params = { "api_action": "avail.kernels" }
+        kernels = self.connection.request(LINODE_ROOT, params=params).object
+        if kernel not in [z["KERNELID"] for z in kernels]:
+            raise LinodeException(0xFB, "Invalid kernel -- avail.kernels")
+
+        # Comments
+        comments = "Created by libcloud <http://www.libcloud.org>" if \
+            "comment" not in kwargs else kwargs["comment"]
+
+        # Labels
+        label = {
+            "lconfig": "[%s] Configuration Profile" % name,
+            "lrecovery": "[%s] Finnix Recovery Configuration" % name,
+            "lroot": "[%s] %s Disk Image" % (name, image.name),
+            "lswap": "[%s] Swap Space" % name
+        }
+        for what in ["lconfig", "lrecovery", "lroot", "lswap"]:
+            if what in kwargs:
+                label[what] = kwargs[what]
+
+        # Step 1: linode.create
+        params = {
+            "api_action":   "linode.create",
+            "DatacenterID": chosen,
+            "PlanID":       size.id,
+            "PaymentTerm":  payment
+        }
+        data = self.connection.request(LINODE_ROOT, params=params).object
+        linode = { "id": data["LinodeID"] }
+
+        # Step 2: linode.disk.createfromdistribution
+        if not root:
+            # Generate a random root password
+            randomness = "!(#%&" + str(Random().random()) + "sup dawg?"
+            root = sha512(randomness).hexdigest()
+        params = {
+            "api_action":       "linode.disk.createfromdistribution",
+            "LinodeID":         linode["id"],
+            "DistributionID":   image.id,
+            "Label":            label["lroot"],
+            "Size":             imagesize,
+            "rootPass":         root,
+        }
+        if ssh: params["rootSSHKey"] = ssh
+        data = self.connection.request(LINODE_ROOT, params=params).object
+        linode["rootimage"] = data["DiskID"]
+
+        # Step 3: linode.disk.create for swap
+        params = {
+            "api_action":       "linode.disk.create",
+            "LinodeID":         linode["id"],
+            "Label":            label["lswap"],
+            "Type":             "swap",
+            "Size":             swap
+        }
+        data = self.connection.request(LINODE_ROOT, params=params).object
+        linode["swapimage"] = data["DiskID"]
+
+        # Step 4: linode.config.create for main profile
+        disks = "%s,%s,,,,,,," % (linode["rootimage"], linode["swapimage"])
+        params = {
+            "api_action":       "linode.config.create",
+            "LinodeID":         linode["id"],
+            "KernelID":         kernel,
+            "Label":            label["lconfig"],
+            "Comments":         comments,
+            "DiskList":         disks
+        }
+        data = self.connection.request(LINODE_ROOT, params=params).object
+        linode["config"] = data["ConfigID"]
+
+        # TODO: Recovery image (Finnix)
+
+        # Step 5: linode.boot
+        params = {
+            "api_action":       "linode.boot",
+            "LinodeID":         linode["id"],
+            "ConfigID":         linode["config"]
+        }
+        data = self.connection.request(LINODE_ROOT, params=params).object
+
+        # Make a node out of it and hand it back
+        params = { "api_action": "linode.list", "LinodeID": linode["id"] }
+        data = self.connection.request(LINODE_ROOT, params=params).object
+        return data[0]
+
     def list_sizes(self):
         # List Sizes
         # Retrieve all available Linode plans.
@@ -171,6 +360,24 @@ class LinodeNodeDriver(NodeDriver):
                 driver=self.connection.driver)
             distros.append(i)
         return distros
+
+    def linode_set_datacenter(self, did):
+        # Set the datacenter for create requests.
+        #
+        # Create will try to guess, based on where all of the API key's Linodes
+        # are located; if they are all in one location, Create will make a new
+        # node there.  If there are NO Linodes on the account or Linodes are in
+        # multiple locations, it is imperative to set this or creates will fail.
+        params = { "api_action": "avail.datacenters" }
+        data = self.connection.request(LINODE_ROOT, params=params).object
+        for dc in data:
+            if did == dc["DATACENTERID"]:
+                self.datacenter = did
+                return
+
+        dcs = ", ".join([d["DATACENTERID"] for d in data])
+        self.datacenter = None
+        raise LinodeException(0xFD, "Invalid datacenter (use one of %s)" % dcs)
 
     def _to_node(self, obj):
         # Convert a returned Linode instance into a Node instance.
