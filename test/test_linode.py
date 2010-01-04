038503dbef1497577e23e1cd9d1ead45ea79f06c
Paul Querna <pquerna@apache.org>
2010-01-04 23:46:25 +0000

    Convert create_node to be kwargs based, updating linode driver to match
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@895828 13f79535-47bb-0310-9956-ffa450edef68

--- test/test_linode.py
+++ test/test_linode.py
@@ -18,7 +18,7 @@
 #
 
 from libcloud.drivers.linode import LinodeNodeDriver
-from libcloud.base import Node, NodeOptions, NodeAuthPassword
+from libcloud.base import Node, NodeAuthPassword
 from test import MockHttp, TestCaseMixin
 
 import unittest
@@ -53,11 +53,11 @@ class LinodeTest(unittest.TestCase, TestCaseMixin):
     
     def test_create_node(self):
         # Will exception on failure
-        no = NodeOptions(location=self.driver.list_locations()[0],
+        node = self.driver.create_node(name="Test",
+                         location=self.driver.list_locations()[0],
                          size=self.driver.list_sizes()[0],
                          image=self.driver.list_images()[6],
-                         auth=NodeAuthPassword("test123"), driver=self.driver)
-        node = self.driver.create_node("Test", no)
+                         auth=NodeAuthPassword("test123"))
     
     def test_list_sizes(self):
         sizes = self.driver.list_sizes()
@@ -71,11 +71,11 @@ class LinodeTest(unittest.TestCase, TestCaseMixin):
         
     def test_create_node_response(self):
         # should return a node object
-        no = NodeOptions(location=self.driver.list_locations()[0],
+        node = self.driver.create_node(name="node-name",
+                         location=self.driver.list_locations()[0],
                          size=self.driver.list_sizes()[0],
                          image=self.driver.list_images()[0],
-                         auth=NodeAuthPassword("foobar"), driver=self.driver)
-        node = self.driver.create_node("node-name", no)
+                         auth=NodeAuthPassword("foobar"))
         self.assertTrue(isinstance(node, Node))
 
         
