98f9ae02c899fc2183249150cf1a585b70fb304c
Paul Querna <pquerna@apache.org>
2010-01-05 04:00:25 +0000

    Improve docs on providers and types
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@895882 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/types.py
+++ libcloud/types.py
@@ -12,21 +12,46 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+Base types used by other parts of libcloud
+"""
+
 class Provider(object):
-    """ Defines for each of the supported providers """
-    DUMMY = 0 # Example provider
-    EC2 = 1 # Amazon AWS
-    EC2_EU = 2 # Amazon AWS EU
-    RACKSPACE = 3 # Cloud Servers
-    SLICEHOST = 4 # Cloud Servers
-    GOGRID = 5 # GoGrid 
-    VPSNET = 6 # VPS.net
-    LINODE = 7 # Linode.com
-    VCLOUD = 8 # vCloud
-    RIMUHOSTING = 9 #RimuHosting.com
+    """
+    Defines for each of the supported providers
+
+    @cvar DUMMY: Example provider
+    @cvar EC2: Amazon AWS
+    @cvar EC2_EU: Amazon AWS EU
+    @cvar RACKSPACE: Rackspace Cloud Servers
+    @cvar SLICEHOST: Slicehost.com
+    @cvar GOGRID: GoGrid
+    @cvar VPSNET: VPS.net
+    @cvar LINODE: Linode.com
+    @cvar VCLOUD: vmware vCloud
+    @cvar RIMUHOSTING: RimuHosting.com
+    """
+    DUMMY = 0
+    EC2 = 1
+    EC2_EU = 2
+    RACKSPACE = 3
+    SLICEHOST = 4
+    GOGRID = 5
+    VPSNET = 6
+    LINODE = 7
+    VCLOUD = 8
+    RIMUHOSTING = 9
 
 class NodeState(object):
-    """ Standard states for a node """
+    """
+    Standard states for a node
+
+    @cvar RUNNING: Node is running
+    @cvar REBOOTING: Node is rebooting
+    @cvar TERMINATED: Node is terminated
+    @cvar PENDING: Node is pending
+    @cvar UNKNOWN: Node state is unknown
+    """
     RUNNING = 0
     REBOOTING = 1
     TERMINATED = 2
@@ -34,6 +59,7 @@ class NodeState(object):
     UNKNOWN = 4
 
 class InvalidCredsException(Exception):
+    """Exception used when invalid credentials are used on a provider."""
     def __init__(self, value='Invalid credentials with the provider'):
         self.value = value
     def __str__(self):
