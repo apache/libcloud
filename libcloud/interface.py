35ae4f948a37468cbefbb98cd1dde769a503ef9d
Paul Querna <pquerna@apache.org>
2010-01-05 04:05:48 +0000

    add docs on interface
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@895886 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/interface.py
+++ libcloud/interface.py
@@ -12,6 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""
+Provides zope.interface definitions for libcloud.
+"""
 from zope.interface import Interface, Attribute
 
 
