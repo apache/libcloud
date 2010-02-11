0d974035919d13a64c49374930b6a10e6fd42e4f
Jerry Chen <jerry@apache.org>
2010-02-11 18:56:21 +0000

    Added unittest.main() for individual testing (w/ PYTHONPATH=..)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@909105 13f79535-47bb-0310-9956-ffa450edef68

--- test/test_rackspace.py
+++ test/test_rackspace.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import sys
 import unittest
 
 from libcloud.types import InvalidCredsException
@@ -142,3 +143,6 @@ class RackspaceMockHttp(MockHttp):
         # only used by destroy node()
         return (httplib.ACCEPTED, "", {}, httplib.responses[httplib.ACCEPTED])
 
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
