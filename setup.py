351c1189e869e2d94f555436af614fd5fb079e02
Paul Querna <pquerna@apache.org>
2010-02-11 00:16:03 +0000

    roll on to 0.3.0-dev
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@908777 13f79535-47bb-0310-9956-ffa450edef68

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ class ApiDocsCommand(Command):
 
 setup(
     name='apache-libcloud',
-    version='0.2.0',
+    version='0.3.0',
     description='A unified interface into many cloud server providers',
     author='Apache Software Foundation',
     author_email='libcloud@incubator.apache.org',
