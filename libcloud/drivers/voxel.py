77443a3e64712bf34471e4cb89752aeec0107861
Gavin McDonald <gmcdonald@apache.org>
2010-03-01 11:50:50 +0000

    patch via email, due to Upayavira
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@917477 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/drivers/voxel.py
+++ libcloud/drivers/voxel.py
@@ -168,8 +168,8 @@ class VoxelNodeDriver(NodeDriver):
                 id = object.findtext("device/id"),
                 name = kwargs["name"],
                 state = NODE_STATE_MAP[object.findtext("devices/status")],
-                public_ip = kwargs.get("public_ip", None),
-                private_ip = kwargs.get("private_ip", None),
+                public_ip = kwargs.get("publicip", None),
+                private_ip = kwargs.get("privateip", None),
                 driver = self.connection.driver
             )
         else:
