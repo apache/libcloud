4d547149957bbb2312bd44182f9eceb790d8f91a
Paul Querna <pquerna@apache.org>
2010-03-01 01:07:12 +0000

    Add Script and Multistep Deployment classes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@917338 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/deployment.py
+++ libcloud/deployment.py
@@ -16,11 +16,12 @@
 """
 Provides generic deployment steps for machines post boot.
 """
+import os
 
 class Deployment(object):
   pass
 
-class SSHDeployment(Deployment):
+class SSHKeyDeployment(Deployment):
   def __init__(self, key):
     self.key = key
   
@@ -32,4 +33,49 @@ class SSHDeployment(Deployment):
     ak.write(self.key)
     ak.close()
     sftp.close()
+    return node
+
+class ScriptDeployment(Deployment):
+  def __init__(self, script, name=None, delete=False):
+    self.script = script
+    self.stdout = None
+    self.stderr = None
+    self.delete = delete
+    self.name = name
+    if self.name is None:
+      self.name = "/root/deployment_%s.sh" % (os.urandom(4).encode('hex'))
+
+  def run(self, node, client):
+    sftp = client.open_sftp()
+    ak = sftp.file(self.name,  mode='w')
+    ak.write(self.script)
+    ak.chmod(755)
+    ak.close()
+    sftp.close()
+
+    stdin, stdout, stderr = client.exec_command(self.name)
+    stdin.close()
+    self.stdout = stdout.read()
+    self.stderr = stderr.read()
+
+    if self.delete:
+      sftp = client.open_sftp()
+      sftp.unlink(self.name)
+      sftp.close()
+
+    return node
+
+class MultiStepDeployment(Deployment):
+  def __init__(self, add = None):
+    self.steps = []
+    self.add(add)
+
+  def add(self, add):
+    if add is not None:
+      add = add if isinstance(add, (list, tuple)) else [add]
+      self.steps.extend(add)
+
+  def run(self, node, client):
+    for s in self.steps:
+      node = s.run(node, client)
     return node
