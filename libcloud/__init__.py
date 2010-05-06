657fb8f78dcd87792afe4455107bd3e953d82330
Paul Querna <pquerna@apache.org>
2010-05-06 22:59:29 +0000

    Roll on to 0.3.2-dev
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@941966 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/__init__.py
+++ libcloud/__init__.py
@@ -21,7 +21,7 @@ libcloud provides a unified interface to the cloud computing resources.
 
 __all__ = ["__version__", "enable_debug"]
 
-__version__ = "0.3.1"
+__version__ = "0.3.2-dev"
 
 
 def enable_debug(fo):
