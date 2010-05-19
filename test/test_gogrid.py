fe90e516f24c23e33303c58567aab61f302c0215
Jerry Chen <jerry@apache.org>
2010-05-19 13:21:30 +0000

    GoGrid tests - LIBCLOUD-34
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@946177 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/test_gogrid.py
@@ -0,0 +1,94 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import sys
+import unittest
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+from libcloud.drivers.gogrid import GoGridNodeDriver
+from libcloud.base import Node, NodeImage, NodeSize
+
+from test import MockHttp, TestCaseMixin
+from test.file_fixtures import FileFixtures
+
+import httplib
+
+class GoGridTests(unittest.TestCase, TestCaseMixin):
+
+    def setUp(self):
+        GoGridNodeDriver.connectionCls.conn_classes = (None, GoGridMockHttp)
+        self.driver = GoGridNodeDriver("foo", "bar")
+
+    def test_create_node(self):
+        image = NodeImage(1531, None, self.driver)
+        size = NodeSize('512Mb', None, None, None, None, None, driver=self.driver)
+
+        node = self.driver.create_node(name='foo', image=image, size=size)
+        self.assertEqual(node.id, 90967)
+
+    def test_list_nodes(self):
+        node = self.driver.list_nodes()[0]
+        self.assertEqual(node.id, 90967)
+
+    def test_reboot_node(self):
+        node = Node(90967, None, None, None, None, self.driver)
+        ret = self.driver.reboot_node(node)
+        self.assertTrue(ret)
+
+    def test_destroy_node(self):
+        node = Node(90967, None, None, None, None, self.driver)
+        ret = self.driver.destroy_node(node)
+        self.assertTrue(ret)
+
+    def test_list_images(self):
+        images = self.driver.list_images()
+        image = images[0]
+        self.assertEqual(len(images), 4)
+        self.assertEqual(image.name, 'CentOS 5.3 (32-bit) w/ None')
+        self.assertEqual(image.id, 1531)
+
+class GoGridMockHttp(MockHttp):
+
+    fixtures = FileFixtures('gogrid')
+
+    def _api_grid_image_list(self, method, url, body, headers):
+        body = self.fixtures.load('image_list.json')
+        return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+
+    def _api_grid_server_list(self, method, url, body, headers):
+        body = self.fixtures.load('server_list.json')
+        return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+
+    def _api_grid_ip_list(self, method, url, body, headers):
+        body = self.fixtures.load('ip_list.json')
+        return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+
+    def _api_grid_server_power(self, method, url, body, headers):
+        body = self.fixtures.load('server_power.json')
+        return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+
+    def _api_grid_server_add(self, method, url, body, headers):
+        body = self.fixtures.load('server_add.json')
+        return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+
+    def _api_grid_server_delete(self, method, url, body, headers):
+        body = self.fixtures.load('server_delete.json')
+        return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+
+if __name__ == '__main__':
+    sys.exit(unittest.main())
