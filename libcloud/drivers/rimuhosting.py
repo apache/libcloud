e7f45722a08f80908e0275dbfc7dab829a11566a
Jerry Chen <jerry@apache.org>
2010-12-25 03:51:04 +0000

    Remove invalid, extraneous copyright notice
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@1052747 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/drivers/rimuhosting.py
+++ libcloud/drivers/rimuhosting.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# Copyright 2009 RedRata Ltd
 """
 RimuHosting Driver
 """
