8494fa8aeea2573950ae43e47171624d7d2aba1f
Jerry Chen <jerry@apache.org>
2011-01-05 04:16:41 +0000

    __version__ to '0.4.1' as well
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@1055292 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/__init__.py
+++ libcloud/__init__.py
@@ -21,7 +21,7 @@ libcloud provides a unified interface to the cloud computing resources.
 
 __all__ = ["__version__", "enable_debug"]
 
-__version__ = "0.4.1-dev"
+__version__ = "0.4.1"
 
 
 def enable_debug(fo):
