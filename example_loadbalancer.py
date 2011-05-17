180a01c6f254b03c2c75ba708531e7a7136f8727
Roman Bogorodskiy <rbogorodskiy@apache.org>
2011-05-17 08:14:47 +0000

    Minor style fixes for loadbalancer example.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/libcloud/trunk@1104046 13f79535-47bb-0310-9956-ffa450edef68

--- example_loadbalancer.py
+++ example_loadbalancer.py
@@ -18,7 +18,7 @@
 import os
 import time
 
-from libcloud.loadbalancer.base import LoadBalancer, Member, Algorithm
+from libcloud.loadbalancer.base import Member, Algorithm
 from libcloud.loadbalancer.types import Provider, State
 from libcloud.loadbalancer.providers import get_driver
 
@@ -28,6 +28,8 @@ def main():
     driver = Rackspace('username', 'api key')
 
     balancers = driver.list_balancers()
+
+    print balancers
 
     # creating a balancer which balances traffic across two
     # nodes: 192.168.86.1:80 and 192.168.86.2:8080. Balancer
@@ -52,14 +54,15 @@ def main():
         if balancer.state == State.RUNNING:
             break
 
+        print "sleeping for 30 seconds for balancers to become ready"
         time.sleep(30)
 
-    # fetch list of nodes
-    nodes = balancer.list_members()
-    print nodes
+    # fetch list of members
+    members = balancer.list_members()
+    print members
 
-    # remove first node
-    balancer.detach_member(nodes[0])
+    # remove first member
+    balancer.detach_member(members[0])
 
     # and add another one: 10.0.0.10:1000
     print balancer.attach_member(Member(None, ip='10.0.0.10', port='1000'))
