083000f6d259dd17b0b8358fe11960bd35b9249c
Tomaž Muraus <tomaz@apache.org>
2011-06-16 19:46:53 +0000

    Add a new compute driver for Skalicloud.com. The driver is based on ElasticStack one.
    git-svn-id: https://svn.apache.org/repos/asf/libcloud/trunk@1136625 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/compute/providers.py
+++ libcloud/compute/providers.py
@@ -45,6 +45,8 @@ DRIVERS = {
         ('libcloud.compute.drivers.elastichosts', 'ElasticHostsUK2NodeDriver'),
     Provider.ELASTICHOSTS_US1:
         ('libcloud.compute.drivers.elastichosts', 'ElasticHostsUS1NodeDriver'),
+    Provider.SKALICLOUD:
+        ('libcloud.compute.drivers.skalicloud', 'SkaliCloudNodeDriver'),
     Provider.CLOUDSIGMA:
         ('libcloud.compute.drivers.cloudsigma', 'CloudSigmaZrhNodeDriver'),
     Provider.GOGRID:
