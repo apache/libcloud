1d46a20c492729bf15d86de1585b137166ba4258
Tomaž Muraus <tomaz@apache.org>
2011-06-17 21:20:58 +0000

    Remove incubator from the link.
    git-svn-id: https://svn.apache.org/repos/asf/libcloud/trunk@1137032 13f79535-47bb-0310-9956-ffa450edef68

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ import libcloud.utils
 libcloud.utils.SHOW_DEPRECATION_WARNING = False
 
 HTML_VIEWSOURCE_BASE = 'https://svn.apache.org/viewvc/libcloud/trunk'
-PROJECT_BASE_DIR = 'http://incubator.apache.org/libcloud/'
+PROJECT_BASE_DIR = 'http://libcloud.apache.org'
 TEST_PATHS = [ 'test', 'test/compute', 'test/storage' , 'test/loadbalancer']
 DOC_TEST_MODULES = [ 'libcloud.compute.drivers.dummy',
                      'libcloud.storage.drivers.dummy' ]
