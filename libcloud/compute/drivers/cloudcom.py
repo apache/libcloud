a4afa6562feebbb3004d99ba1731539f44ed9b1d
gigimon <gigimon4ik@gmail.com>
2011-09-08 16:34:32 +0300

    Changes in create_node in cloud.com

--- libcloud/compute/drivers/cloudcom.py
+++ libcloud/compute/drivers/cloudcom.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 from libcloud.compute.providers import Provider
-
+from libcloud.compute.base import Node
 from libcloud.compute.drivers.cloudstack import CloudStackNodeDriver
 
 class CloudComNodeDriver(CloudStackNodeDriver):
@@ -29,3 +29,37 @@ class CloudComNodeDriver(CloudStackNodeDriver):
     def __init__(self, key, secret=None, secure=False, host=None, port=None):
         host = host or self.host
         super(CloudComNodeDriver, self).__init__(key, secret, secure, host, port)
+
+
+    def create_node(self, name, size, image, location=None, **kwargs):
+        if location is None:
+            location = self.list_locations()[0]
+        network_id = kwargs.pop('network_id', None)
+        if network_id is None:
+            networks = self._sync_request('listNetworks')
+            network_id = networks['network'][0]['id']
+        result = self._async_request('deployVirtualMachine',
+                                     name=name,
+                                     displayname=name,
+                                     serviceOfferingId=size.id,
+                                     templateId=image.id,
+                                     zoneId=location.id,
+                                     networkIds=network_id,
+                                    )
+
+        node = result['virtualmachine']
+
+        return Node(
+            id=node['id'],
+            name=node['displayname'],
+            state=self.NODE_STATE_MAP[node['state']],
+            public_ip=[],
+            private_ip=[x['ipaddress'] for x in node['nic']],
+            driver=self,
+            extra={
+                   'zoneid': location.id,
+                   'ip_addresses': [],
+                   'forwarding_rules': [],
+                   'password': node['password'],
+                   }
+                )
