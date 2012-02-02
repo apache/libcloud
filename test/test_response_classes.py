9ca1b31fe25e602605b2804936b623157b338272
Tomaž Muraus <tomaz@apache.org>
2012-02-02 06:48:32 +0000

    Add support for decompressing responses inside the base Response class. Also add tests.
    git-svn-id: https://svn.apache.org/repos/asf/libcloud/trunk@1239468 13f79535-47bb-0310-9956-ffa450edef68

--- test/test_response_classes.py
+++ test/test_response_classes.py
@@ -15,11 +15,13 @@
 
 import sys
 import unittest
+import zlib
+import gzip
 
 from mock import Mock
 
-from libcloud.utils.py3 import httplib
-from libcloud.common.base import XmlResponse, JsonResponse
+from libcloud.utils.py3 import httplib, b, StringIO, PY3
+from libcloud.common.base import Response, XmlResponse, JsonResponse
 from libcloud.common.types import MalformedResponseError
 
 
@@ -86,6 +88,63 @@ class ResponseClassesTests(unittest.TestCase):
 
         parsed = response.parse_body()
         self.assertEqual(parsed, '')
+
+    def test_deflate_encoding(self):
+        original_data = 'foo bar ponies, wooo zlib'
+        compressed_data = zlib.compress(b(original_data))
+
+        self._mock_response.read.return_value = compressed_data
+        self._mock_response.getheaders.return_value = \
+                {'Content-Encoding': 'deflate'}
+
+        response = Response(response=self._mock_response,
+                            connection=self._mock_connection)
+
+        body = response.parse_body()
+        self.assertEqual(body, original_data)
+
+        self._mock_response.getheaders.return_value = \
+                {'Content-Encoding': 'zlib'}
+
+        response = Response(response=self._mock_response,
+                            connection=self._mock_connection)
+
+        body = response.parse_body()
+        self.assertEqual(body, original_data)
+
+    def test_gzip_encoding(self):
+        original_data = 'foo bar ponies, wooo gzip'
+
+        if PY3:
+            from io import BytesIO
+            string_io = BytesIO()
+        else:
+            string_io = StringIO()
+
+        stream = gzip.GzipFile(fileobj=string_io, mode='w')
+        stream.write(b(original_data))
+        stream.close()
+        compressed_data = string_io.getvalue()
+
+        self._mock_response.read.return_value = compressed_data
+        self._mock_response.getheaders.return_value = \
+                {'Content-Encoding': 'gzip'}
+
+        response = Response(response=self._mock_response,
+                            connection=self._mock_connection)
+
+        body = response.parse_body()
+        self.assertEqual(body, original_data)
+
+        self._mock_response.getheaders.return_value = \
+                {'Content-Encoding': 'x-gzip'}
+
+        response = Response(response=self._mock_response,
+                            connection=self._mock_connection)
+
+        body = response.parse_body()
+        self.assertEqual(body, original_data)
+
 
 
 if __name__ == '__main__':
