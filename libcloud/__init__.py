b2952ea42338d9a5bbc7bcd262f8bd59d338cc46
Tomaž Muraus <tomaz@apache.org>
2012-02-04 06:27:36 +0000

    bump version.
    git-svn-id: https://svn.apache.org/repos/asf/libcloud/trunk@1240457 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/__init__.py
+++ libcloud/__init__.py
@@ -20,7 +20,7 @@ libcloud provides a unified interface to the cloud computing resources.
 """
 
 __all__ = ["__version__", "enable_debug"]
-__version__ = '0.7.1'
+__version__ = '0.8.0'
 
 try:
     import paramiko
