295dbc10d444e359cec648ce3af047ce90dcf789
Tomaž Muraus <tomaz@apache.org>
2013-02-08 11:52:54 +0000

    Python 2.5 fix.
    git-svn-id: https://svn.apache.org/repos/asf/libcloud/trunk@1443962 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/storage/drivers/azure_blobs.py
+++ libcloud/storage/drivers/azure_blobs.py
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import with_statement
+
 import time
 import base64
 import hmac
