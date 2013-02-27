e13eb88822cd6425f8319eb4674f4454a2700848
Tomaž Muraus <tomaz@apache.org>
2013-02-27 22:57:44 +0000

    Merge changes from 0.12.x.
    git-svn-id: https://svn.apache.org/repos/asf/libcloud/trunk@1451014 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/__init__.py
+++ libcloud/__init__.py
@@ -20,7 +20,7 @@ libcloud provides a unified interface to the cloud computing resources.
 """
 
 __all__ = ['__version__', 'enable_debug']
-__version__ = '0.12.0-dev'
+__version__ = '0.12.2-dev'
 
 try:
     import paramiko
