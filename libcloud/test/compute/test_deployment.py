0db427e15c5d952d22163ba41fc7d94c46c4e55c
Tomaž Muraus <tomaz@apache.org>
2013-03-29 17:59:58 +0000

    Fix python 2.5 test failure.
    git-svn-id: https://svn.apache.org/repos/asf/libcloud/trunk@1462561 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/test/compute/test_deployment.py
+++ libcloud/test/compute/test_deployment.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import with_statement
+
 import os
 import sys
 import time
