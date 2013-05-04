9475d0bdf8bf6e667aa61a5cbf8ba29770983e7a
Tomaž Muraus <tomaz@apache.org>
2013-05-04 05:55:30 +0000

    Various improvements and bug-fixes in the VCloud driver.
    Contributed by Michel Samia, part of LIBCLOUD-323.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/libcloud/trunk@1479041 13f79535-47bb-0310-9956-ffa450edef68

--- libcloud/test/compute/test_vcloud.py
+++ libcloud/test/compute/test_vcloud.py
@@ -22,6 +22,7 @@ from libcloud.utils.py3 import httplib, b
 from libcloud.compute.drivers.vcloud import TerremarkDriver, VCloudNodeDriver, Subject
 from libcloud.compute.drivers.vcloud import VCloud_1_5_NodeDriver, ControlAccess
 from libcloud.compute.drivers.vcloud import VCloud_5_1_NodeDriver
+from libcloud.compute.drivers.vcloud import Vdc
 from libcloud.compute.base import Node, NodeImage
 from libcloud.compute.types import NodeState
 
@@ -242,6 +243,13 @@ class VCloud_1_5_Tests(unittest.TestCase, TestCaseMixin):
     def test_ex_list_nodes(self):
         self.assertEqual(len(self.driver.ex_list_nodes()), len(self.driver.list_nodes()))
 
+    def test_ex_list_nodes__masked_exception(self):
+        """
+        Test that we don't mask other exceptions.
+        """
+        brokenVdc = Vdc('/api/vdc/brokenVdc', 'brokenVdc', self.driver)
+        self.assertRaises(AnotherError, self.driver.ex_list_nodes, (brokenVdc))
+
     def test_ex_power_off(self):
         node = Node('https://vm-vcloud/api/vApp/vapp-8c57a5b6-e61b-48ca-8a78-3b70ee65ef6b', 'testNode', NodeState.RUNNING, [], [], self.driver)
         self.driver.ex_power_off_node(node)
@@ -398,6 +406,21 @@ class TerremarkMockHttp(MockHttp):
         return (httplib.ACCEPTED, body, headers, httplib.responses[httplib.ACCEPTED])
 
 
+class AnotherErrorMember(Exception):
+    """
+    helper class for the synthetic exception
+    """
+
+    def __init__(self):
+        self.tag = 'Error'
+
+    def get(self, foo):
+        return 'ACCESS_TO_RESOURCE_IS_FORBIDDEN'
+
+class AnotherError(Exception):
+    pass
+
+
 class VCloud_1_5_MockHttp(MockHttp, unittest.TestCase):
 
     fixtures = ComputeFileFixtures('vcloud_1_5')
@@ -430,6 +453,14 @@ class VCloud_1_5_MockHttp(MockHttp, unittest.TestCase):
         body = self.fixtures.load('api_vdc_3d9ae28c_1de9_4307_8107_9356ff8ba6d0.xml')
         return httplib.OK, body, headers, httplib.responses[httplib.OK]
 
+    def _api_vdc_brokenVdc(self, method, url, body, headers):
+        body = self.fixtures.load('api_vdc_brokenVdc.xml')
+        return httplib.OK, body, headers, httplib.responses[httplib.OK]
+
+    def _api_vApp_vapp_errorRaiser(self, method, url, body, headers):
+        m = AnotherErrorMember()
+        raise AnotherError(m)
+
     def _api_vdc_3d9ae28c_1de9_4307_8107_9356ff8ba6d0_action_instantiateVAppTemplate(self, method, url, body, headers):
         body = self.fixtures.load('api_vdc_3d9ae28c_1de9_4307_8107_9356ff8ba6d0_action_instantiateVAppTemplate.xml')
         return httplib.ACCEPTED, body, headers, httplib.responses[httplib.ACCEPTED]
@@ -458,6 +489,10 @@ class VCloud_1_5_MockHttp(MockHttp, unittest.TestCase):
 
     def _api_vApp_vapp_8c57a5b6_e61b_48ca_8a78_3b70ee65ef6b(self, method, url, body, headers):
         body = self.fixtures.load('api_vApp_vapp_8c57a5b6_e61b_48ca_8a78_3b70ee65ef6b.xml')
+        return httplib.OK, body, headers, httplib.responses[httplib.OK]
+
+    def _api_vApp_vapp_8c57a5b6_e61b_48ca_8a78_3b70ee65ef6c(self, method, url, body, headers):
+        body = self.fixtures.load('api_vApp_vapp_8c57a5b6_e61b_48ca_8a78_3b70ee65ef6c.xml')
         return httplib.OK, body, headers, httplib.responses[httplib.OK]
 
     def _api_vApp_vm_dd75d1d3_5b7b_48f0_aff3_69622ab7e045(self, method, url, body, headers):
