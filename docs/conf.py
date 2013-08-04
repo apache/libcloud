e06179862d25754691b1ef4b5481e3f94cc748ce
Tomaz Muraus <tomaz@apache.org>
2013-08-04 02:23:39 +0200

    docs: Enable sphinx.ext.viewcode extenstion.

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,8 @@ sys.path.insert(0, os.path.abspath('../'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
+              'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
