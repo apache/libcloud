c4b3daae946049652a500a8515929b4cbf14a6b4
Tomaz Muraus <tomaz@apache.org>
2013-09-13 15:28:03 +0200

    Bump version.

--- libcloud/__init__.py
+++ libcloud/__init__.py
@@ -20,7 +20,7 @@ libcloud provides a unified interface to the cloud computing resources.
 """
 
 __all__ = ['__version__', 'enable_debug']
-__version__ = '0.13.1'
+__version__ = '0.13.2'
 
 import os
 import atexit
