519716edc77ab438be18a2d86fc83317a4da658a
Tomaz Muraus <tomaz@apache.org>
2013-09-18 19:45:46 +0200

    docs: Update config, also include documentation for __init__ method.

--- docs/conf.py
+++ docs/conf.py
@@ -250,3 +250,5 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+autoclass_content = 'both'
