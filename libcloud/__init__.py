36dba171d1c577c727852b515b932036bc06d382
Tomaz Muraus <tomaz@apache.org>
2013-10-22 11:18:30 +0200

    Bump version to 0.14.0-beta2.

--- libcloud/__init__.py
+++ libcloud/__init__.py
@@ -20,7 +20,7 @@ libcloud provides a unified interface to the cloud computing resources.
 """
 
 __all__ = ['__version__', 'enable_debug']
-__version__ = '0.14.0-beta1'
+__version__ = '0.14.0-beta2'
 
 import os
 
