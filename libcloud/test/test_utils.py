e021fd227a1ebdd67fa5868f789ff2560a35747d
Tomaz Muraus <tomaz@apache.org>
2013-12-04 01:40:52 +0100

    Add tests for is_public_subnet and is_private_subnet function.

--- libcloud/test/test_utils.py
+++ libcloud/test/test_utils.py
@@ -37,6 +37,8 @@ from libcloud.utils.py3 import urlquote
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import DRIVERS
 from libcloud.utils.misc import get_secure_random_string
+from libcloud.utils.networking import is_public_subnet
+from libcloud.utils.networking import is_private_subnet
 from libcloud.utils.networking import is_valid_ip_address
 
 
@@ -238,6 +240,34 @@ class TestUtils(unittest.TestCase):
 
 
 class NetworkingUtilsTestCase(unittest.TestCase):
+    def test_is_public_and_is_private_subnet(self):
+        public_ips = [
+            '213.151.0.8',
+            '86.87.86.1',
+            '8.8.8.8',
+            '8.8.4.4'
+        ]
+
+        private_ips = [
+            '192.168.1.100',
+            '10.0.0.1',
+            '172.16.0.0'
+        ]
+
+        for address in public_ips:
+            is_public = is_public_subnet(ip=address)
+            is_private = is_private_subnet(ip=address)
+
+            self.assertTrue(is_public)
+            self.assertFalse(is_private)
+
+        for address in private_ips:
+            is_public = is_public_subnet(ip=address)
+            is_private = is_private_subnet(ip=address)
+
+            self.assertFalse(is_public)
+            self.assertTrue(is_private)
+
     def test_is_valid_ip_address(self):
         valid_ipv4_addresses = [
             '192.168.1.100',
