04bf60445eaf05a2c2ba5eb9c63d605e1e8c7f2b
John Obelenus <jobelenus@gmail.com>
2013-12-05 17:44:37 -0500

    just doing this for a speed test, will revert later

--- libcloud/storage/drivers/cloudfiles.py
+++ libcloud/storage/drivers/cloudfiles.py
@@ -136,8 +136,8 @@ class CloudFilesConnection(OpenStackBaseConnection):
         if not ep:
             raise LibcloudError('Could not find specified endpoint')
 
-        if 'publicURL' in ep:
-            return ep['publicURL']
+        if 'internalURL' in ep:
+            return ep['internalURL']
         else:
             raise LibcloudError('Could not find specified endpoint')
 
