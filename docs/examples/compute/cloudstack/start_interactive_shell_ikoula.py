ce4ee9558ed0c53e03cb31627c597d5388b56ffa
Tomaz Muraus <tomaz@apache.org>
2013-12-13 13:49:12 +0100

    docs: Update example, no need to disable cert validation.

--- docs/examples/compute/cloudstack/start_interactive_shell_ikoula.py
+++ docs/examples/compute/cloudstack/start_interactive_shell_ikoula.py
@@ -4,9 +4,6 @@ from IPython.terminal.embed import InteractiveShellEmbed
 
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
-import libcloud.security as sec
-
-sec.VERIFY_SSL_CERT = False
 
 apikey = os.getenv('IKOULA_API_KEY')
 secretkey = os.getenv('IKOULA_SECRET_KEY')
