509040a68b5575e6785e6c085e76055cdc1da0fa
Tomaz Muraus <tomaz@apache.org>
2014-01-06 17:34:42 +0100

    Include server side error message in the multipart error exception.

--- libcloud/storage/drivers/s3.py
+++ libcloud/storage/drivers/s3.py
@@ -64,7 +64,7 @@ RESPONSES_PER_REQUEST = 100
 
 
 class S3Response(AWSBaseResponse):
-
+    namespace = None
     valid_response_codes = [httplib.NOT_FOUND, httplib.CONFLICT,
                             httplib.BAD_REQUEST]
 
@@ -588,7 +588,10 @@ class S3StorageDriver(StorageDriver):
                                            method='POST')
 
         if response.status != httplib.OK:
-            raise LibcloudError('Error in multipart commit', driver=self)
+            element = response.object
+            code, message = response._parse_error_details(element=element)
+            msg = 'Error in multipart commit: %s (%s)' % (message, code)
+            raise LibcloudError(msg, driver=self)
 
         # Get the server's etag to be passed back to the caller
         body = response.parse_body()
