0b1e4af01dcec3add29be48760d280f5c9f5a144
Rick Wright <rickw@google.com>
2014-01-17 05:33:10 -0800

    Lint fixes

--- docs/examples/compute/gce/gce_datacenter.py
+++ docs/examples/compute/gce/gce_datacenter.py
@@ -5,5 +5,5 @@ ComputeEngine = get_driver(Provider.GCE)
 # Datacenter is set to 'us-central1-a' as an example, but can be set to any
 # zone, like 'us-central1-b' or 'europe-west1-a'
 driver = ComputeEngine('your_service_account_email', 'path_to_pem_file',
-                        datacenter='us-central1-a',
-                        project='your_project_id')
+                       datacenter='us-central1-a',
+                       project='your_project_id')
--- docs/examples/compute/gce/gce_service_account.py
+++ docs/examples/compute/gce/gce_service_account.py
@@ -3,4 +3,4 @@ from libcloud.compute.providers import get_driver
 
 ComputeEngine = get_driver(Provider.GCE)
 driver = ComputeEngine('your_service_account_email', 'path_to_pem_file',
-                        project='your_project_id')
+                       project='your_project_id')
