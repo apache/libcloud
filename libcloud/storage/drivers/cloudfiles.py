34c3df43ddb37d077cfb40049753d6ff487a2e91
John Obelenus <jobelenus@gmail.com>
2014-01-29 18:31:45 -0500

    When a request is made to a CDN endpoint there is no internalURL to use, we have to fall back to the publicURL
    Closes #231
    
    Signed-off-by: Tomaz Muraus <tomaz@apache.org>

--- libcloud/storage/drivers/cloudfiles.py
+++ libcloud/storage/drivers/cloudfiles.py
@@ -52,6 +52,8 @@ from libcloud.common.rackspace import AUTH_URL
 
 CDN_HOST = 'cdn.clouddrive.com'
 API_VERSION = 'v1.0'
+INTERNAL_ENDPOINT_KEY = 'internalURL'
+PUBLIC_ENDPOINT_KEY = 'publicURL'
 
 
 class CloudFilesResponse(Response):
@@ -113,7 +115,13 @@ class CloudFilesConnection(OpenStackBaseConnection):
         self.api_version = API_VERSION
         self.accept_format = 'application/json'
         self.cdn_request = False
-        self.endpoint_url = 'internalURL' if use_internal_url else 'publicURL'
+        self.use_internal_url = use_internal_url
+
+    def _get_endpoint_key(self):
+        endpoint_key = INTERNAL_ENDPOINT_KEY if self.use_internal_url else PUBLIC_ENDPOINT_KEY
+        if self.cdn_request:
+            endpoint_key = PUBLIC_ENDPOINT_KEY  # cdn endpoints don't have internal urls
+        return endpoint_key
 
     def get_endpoint(self):
         region = self._ex_force_service_region.upper()
@@ -134,12 +142,13 @@ class CloudFilesConnection(OpenStackBaseConnection):
         # if this is a CDN request, return the cdn url instead
         if self.cdn_request:
             ep = cdn_ep
+        endpoint_url = self._get_endpoint_key()
 
         if not ep:
             raise LibcloudError('Could not find specified endpoint')
 
-        if self.endpoint_url in ep:
-            return ep[self.endpoint_url]
+        if endpoint_url in ep:
+            return ep[endpoint_url]
         else:
             raise LibcloudError('Could not find specified endpoint')
 
