324303a423527786403812a26d9177d60b6afd78
briancurtin <brian.curtin@rackspace.com>
2014-01-29 22:50:46 -0600

    Add 'name' argument to key pair importing examples
    Closes #232.
    
    Signed-off-by: Tomaz Muraus <tomaz@apache.org>

--- docs/examples/compute/import_key_pair_from_file.py
+++ docs/examples/compute/import_key_pair_from_file.py
@@ -10,4 +10,4 @@ Driver = get_driver(Provider.EC2)
 conn = Driver(EC2_ACCESS_ID, EC2_SECRET_KEY)
 
 key_file_path = os.path.expanduser('~/.ssh/id_rsa_my_key_pair_1.pub')
-key_pair = conn.import_key_pair_from_file(key_file_path=key_file_path)
+key_pair = conn.import_key_pair_from_file(name='my_key', key_file_path=key_file_path)
