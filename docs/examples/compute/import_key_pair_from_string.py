37abf99c55303a7b9b59050e324cd603ba30ff7f
Tomaz Muraus <tomaz@apache.org>
2014-01-30 12:13:00 +0100

    Fix lint.

--- docs/examples/compute/import_key_pair_from_string.py
+++ docs/examples/compute/import_key_pair_from_string.py
@@ -12,7 +12,9 @@ Driver = get_driver(Provider.EC2)
 conn = Driver(EC2_ACCESS_ID, EC2_SECRET_KEY)
 
 key_file_path = os.path.expanduser('~/.ssh/id_rsa_my_key_pair_1.pub')
+
 with open(key_file_path, 'r') as fp:
     key_material = fp.read()
 
-key_pair = conn.import_key_pair_from_string(name='my_key', key_material=key_material)
+key_pair = conn.import_key_pair_from_string(name='my_key',
+                                            key_material=key_material)
