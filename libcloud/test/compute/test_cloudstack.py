4fca0e9da4cba729985ce46b0bc40c48bfeda04d
Roeland Kuipers <rkuipers@schubergphilis.com>
2014-06-11 09:00:20 +0200

    fix long lines

--- libcloud/test/compute/test_cloudstack.py
+++ libcloud/test/compute/test_cloudstack.py
@@ -199,17 +199,23 @@ class CloudStackCommonTestCase(TestCaseMixin):
     def test_ex_list_network_offerings(self):
         _, fixture = CloudStackMockHttp()._load_fixture(
             'listNetworkOfferings_default.json')
-        fixture_networkoffers = fixture['listnetworkofferingsresponse']['networkoffering']
+        fixture_networkoffers = fixture
+        ['listnetworkofferingsresponse']['networkoffering']
 
         networkoffers = self.driver.ex_list_network_offerings()
 
         for i, networkoffer in enumerate(networkoffers):
             self.assertEqual(networkoffer.id, fixture_networkoffers[i]['id'])
-            self.assertEqual(networkoffer.name, fixture_networkoffers[i]['name'])
-            self.assertEqual(networkoffer.displaytext, fixture_networkoffers[i]['displaytext'])
-            self.assertEqual(networkoffer.forvpc, fixture_networkoffers[i]['forvpc'])
-            self.assertEqual(networkoffer.guestiptype, fixture_networkoffers[i]['guestiptype'])
-            self.assertEqual(networkoffer.serviceofferingid, fixture_networkoffers[i]['serviceofferingid'])
+            self.assertEqual(networkoffer.name,
+                             fixture_networkoffers[i]['name'])
+            self.assertEqual(networkoffer.displaytext,
+                             fixture_networkoffers[i]['displaytext'])
+            self.assertEqual(networkoffer.forvpc,
+                             fixture_networkoffers[i]['forvpc'])
+            self.assertEqual(networkoffer.guestiptype,
+                             fixture_networkoffers[i]['guestiptype'])
+            self.assertEqual(networkoffer.serviceofferingid,
+                             fixture_networkoffers[i]['serviceofferingid'])
 
     def test_ex_create_network(self):
         _, fixture = CloudStackMockHttp()._load_fixture(
