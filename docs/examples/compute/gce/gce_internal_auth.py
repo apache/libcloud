bca920d19282c12eb60f0e71e87dfeab1e4b1607
Eric Johnson <erjohnso@google.com>
2015-01-23 22:32:26 +0000

    [google compute] add support for JSON private key format

--- docs/examples/compute/gce/gce_internal_auth.py
+++ docs/examples/compute/gce/gce_internal_auth.py
@@ -2,8 +2,11 @@ from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 
 # This example assumes you are running on an instance within Google
-# Compute Engine. As such, the only parameter you need to specify is
+# Compute Engine. As such, the only value you need to specify is
 # the Project ID. The GCE driver will the consult GCE's internal
 # metadata service for an authorization token.
+#
+# You must still place placeholder empty strings for user_id / key
+# due to the nature of the driver's __init__() params.
 ComputeEngine = get_driver(Provider.GCE)
-driver = ComputeEngine(project='your_project_id')
+driver = ComputeEngine('', '', project='your_project_id')
--- docs/examples/compute/gce/gce_service_account.py
+++ docs/examples/compute/gce/gce_service_account.py
@@ -2,5 +2,7 @@ from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 
 ComputeEngine = get_driver(Provider.GCE)
+# Note that the 'PEM file' argument can either be the JSON format or
+# the P12 format.
 driver = ComputeEngine('your_service_account_email', 'path_to_pem_file',
                        project='your_project_id')
