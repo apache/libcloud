7bfe6d36fd9f73b313bf128dda51d0b140e3ca4e
Wido den Hollander <wido@widodh.nl>
2015-03-05 22:39:34 +0100

    LIBCLOUD-641: The Aurora Compute libcloud driver
    Aurora Compute is the CloudStack based public cloud offering from
    PCextreme B.V., a Dutch hosting provider.

--- libcloud/compute/providers.py
+++ libcloud/compute/providers.py
@@ -153,6 +153,8 @@ DRIVERS = {
     ('libcloud.compute.drivers.profitbricks', 'ProfitBricksNodeDriver'),
     Provider.VULTR:
     ('libcloud.compute.drivers.vultr', 'VultrNodeDriver'),
+    Provider.AURORACOMPUTE:
+    ('libcloud.compute.drivers.auroracompute', 'AuroraComputeNodeDriver'),
 
     # Deprecated
     Provider.CLOUDSIGMA_US:
