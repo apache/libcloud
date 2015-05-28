af15645b556a32ec65d6c66805754bd5d9d48650
jcastillo2nd <j.castillo.2nd@gmail.com>
2015-05-28 13:46:18 +0000

    DigitalOceanNodeDriver minor revisions - Raise on api_version / key or key-secret mismatch - Added tests for v1 and v2 DigitalOceanNodeDriver using wrong keys - Updated examples to include explicit api_version values
    Signed-off-by: Tomaz Muraus <tomaz@tomaz.me>

--- docs/examples/compute/digitalocean/instantiate_api_v1.0.py
+++ docs/examples/compute/digitalocean/instantiate_api_v1.0.py
@@ -2,4 +2,4 @@ from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 
 cls = get_driver(Provider.DIGITAL_OCEAN)
-driver = cls('client id', 'api key')
+driver = cls('client id', 'api key', api_version='v1')
