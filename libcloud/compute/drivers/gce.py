53a4cf575e409aff6780f6ab80ff55768f5333ce
Marcin Zaremba <mzaremba@egnyte.com>
2015-07-22 12:37:14 +0200

    By default let's assume if it's not string it's a Node object

--- libcloud/compute/drivers/gce.py
+++ libcloud/compute/drivers/gce.py
@@ -2187,7 +2187,6 @@ class GCENodeDriver(NodeDriver):
         elif isinstance(next_hop, str):
             route_data['nextHopIp'] = next_hop
         else:
-            node = self.ex_get_node(next_hop)
             route_data['nextHopInstance'] = node.extra['selfLink']
 
         request = '/global/routes'
