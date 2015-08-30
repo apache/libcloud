c26a9b0202961661fb31f2449e7a606597b02ad9
Anthony Shaw <anthony.p.shaw@gmail.com>
2015-08-30 22:25:30 +1000

    [LIBCLOUD-737] Fixed module path for compute driver tests

--- libcloud/test/compute/test_dimensiondata.py
+++ libcloud/test/compute/test_dimensiondata.py
@@ -18,8 +18,8 @@ from libcloud.utils.py3 import httplib
 
 from libcloud.common.types import InvalidCredsError
 from libcloud.compute.drivers.dimensiondata import DimensionDataNodeDriver as DimensionData
-from libcloud.compute.drivers.dimensiondata import DimensionDataAPIException
 from libcloud.compute.base import Node, NodeAuthPassword, NodeLocation
+from libcloud.common.dimensiondata import DimensionDataAPIException
 
 from libcloud.test import MockHttp
 from libcloud.test.compute import TestCaseMixin
