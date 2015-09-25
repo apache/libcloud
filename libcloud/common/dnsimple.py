0744dba6a52c42dbe6f9ba360e5311a1f90c3550
Tomaz Muraus <tomaz@apache.org>
2015-09-25 14:50:56 +0200

    Fix python 3 compatibility issue in DNSimple driver.

--- libcloud/common/dnsimple.py
+++ libcloud/common/dnsimple.py
@@ -12,7 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import httplib
+
+from libcloud.utils.py3 import httplib
 from libcloud.common.base import ConnectionUserAndKey
 from libcloud.common.base import JsonResponse
 
