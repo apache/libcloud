3bcd60a55ab633d8c46efbf95c0fed72a486136d
Arindam Choudhury <arindam@live.com>
2015-10-20 12:14:52 +0200

    key_file should be
    According to https://azure.microsoft.com/en-us/documentation/articles/cloud-services-python-how-to-use-service-management/ key_file should be '/path/to/azure_cert.pem'
    azure_cert.cer raises ssl.SSLError: [SSL] PEM lib (_ssl.c:2580) error.
    
    Closes #611
    
    Signed-off-by: Tomaz Muraus <tomaz@apache.org>

--- docs/examples/compute/azure/instantiate.py
+++ docs/examples/compute/azure/instantiate.py
@@ -3,4 +3,4 @@ from libcloud.compute.providers import get_driver
 
 cls = get_driver(Provider.AZURE)
 driver = cls(subscription_id='subscription-id',
-             key_file='/path/to/azure_cert.cer')
+             key_file='/path/to/azure_cert.pem')
