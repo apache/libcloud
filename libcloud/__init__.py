fd02006e651459a938f3d9c2bdf204837500769e
Tomaz Muraus <tomaz@tomaz.me>
2015-11-08 22:13:58 +0100

    Indicate we are now working on a new release.

--- libcloud/__init__.py
+++ libcloud/__init__.py
@@ -20,7 +20,7 @@ libcloud provides a unified interface to the cloud computing resources.
 """
 
 __all__ = ['__version__', 'enable_debug']
-__version__ = '0.19.0'
+__version__ = '0.19.1-dev'
 
 import os
 import codecs
