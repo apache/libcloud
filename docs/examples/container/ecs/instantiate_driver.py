4bde5af3e2ead0e81763805eadeacc0451f334f2
anthony-shaw <anthony.p.shaw@gmail.com>
2016-01-03 20:50:43 +1100

    removed local junk from example code, included real fixtures from actual API

--- docs/examples/container/ecs/instantiate_driver.py
+++ docs/examples/container/ecs/instantiate_driver.py
@@ -1,12 +1,12 @@
 from libcloud.container.types import Provider
 from libcloud.container.providers import get_driver
 
-import libcloud.security
-libcloud.security.VERIFY_SSL_CERT = False
-
 cls = get_driver(Provider.ECS)
 
 conn = cls(access_id='AKIAI7OR4GWEEPRIFBBA', secret='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', region='ap-southeast-2')
-conn.connection.set_http_proxy(proxy_url='http://localhost:8888')
 
-print(conn.list_containers())
+for container in conn.list_containers():
+    print(container.name)
+
+for cluster in conn.list_clusters():
+    print(cluster.name)
--- libcloud/test/container/test_ecs.py
+++ libcloud/test/container/test_ecs.py
@@ -55,12 +55,12 @@ class ElasticContainerDriverTestCase(unittest.TestCase):
 
     def test_list_containers(self):
         containers = self.driver.list_containers()
-        self.assertEqual(len(containers), 2)
+        self.assertEqual(len(containers), 1)
 
     def test_list_containers_for_cluster(self):
         cluster = self.driver.list_clusters()[0]
         containers = self.driver.list_containers(cluster=cluster)
-        self.assertEqual(len(containers), 2)
+        self.assertEqual(len(containers), 1)
 
     def test_deploy_container(self):
         container = self.driver.deploy_container(
@@ -79,9 +79,9 @@ class ElasticContainerDriverTestCase(unittest.TestCase):
         container = self.driver.get_container(
             'arn:aws:ecs:us-east-1:012345678910:container/76c980a8-2454-4a9c-acc4-9eb103117273'
         )
-        self.assertEqual(container.id, 'arn:aws:ecs:us-east-1:012345678910:container/76c980a8-2454-4a9c-acc4-9eb103117273')
-        self.assertEqual(container.name, 'mysql')
-        self.assertEqual(container.image.name, 'mysql')
+        self.assertEqual(container.id, 'arn:aws:ecs:ap-southeast-2:647433528374:container/d56d4e2c-9804-42a7-9f2a-6029cb50d4a2')
+        self.assertEqual(container.name, 'simple-app')
+        self.assertEqual(container.image.name, 'simple-app')
 
     def test_start_container(self):
         container = self.driver.start_container(
