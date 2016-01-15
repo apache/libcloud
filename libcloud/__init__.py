d1cf931796f1064f5b1c36d35a80551488fb5d50
Eric Johnson <erjohnso@google.com>
2016-01-15 15:39:46 +0000

    bugfix version 0.20.1

--- libcloud/__init__.py
+++ libcloud/__init__.py
@@ -20,7 +20,7 @@ libcloud provides a unified interface to the cloud computing resources.
 """
 
 __all__ = ['__version__', 'enable_debug']
-__version__ = '0.20.0'
+__version__ = '0.20.1'
 
 import os
 import codecs
