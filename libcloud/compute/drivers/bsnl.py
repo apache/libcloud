2df4c0fda38d3dd7a301e8b5d150e3c77d8c4462
Jeffrey Dunham <jeffrey.a.dunham@gmail.com>
2016-02-02 21:07:07 -0500

    Fixing docs build issue

--- libcloud/compute/drivers/bsnl.py
+++ libcloud/compute/drivers/bsnl.py
@@ -12,9 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""
-BSNL Driver
-"""
 
 from libcloud.compute.providers import Provider
 from libcloud.common.dimensiondata import (DimensionDataConnection,
