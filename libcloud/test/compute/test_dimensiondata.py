ba3107c3cfcddbb140b007b4b8e8b54169a5f40b
Jeffrey Dunham <jeffrey.a.dunham@gmail.com>
2016-02-06 01:44:58 -0500

    DimensionData: changing image to be NodeImage or str, upping code coverage in tests

--- libcloud/test/compute/test_dimensiondata.py
+++ libcloud/test/compute/test_dimensiondata.py
@@ -143,12 +143,42 @@ class DimensionDataTests(unittest.TestCase, TestCaseMixin):
         self.assertEqual(node.id, 'e75ead52-692f-4314-8725-c8a4f4d13a87')
         self.assertEqual(node.extra['status'].action, 'DEPLOY_SERVER')
 
+    def test_create_node_response_STR(self):
+        rootPw = 'pass123'
+        image = self.driver.list_images()[0].id
+        network = self.driver.ex_list_networks()[0].id
+        node = self.driver.create_node(name='test2', image=image, auth=rootPw,
+                                       ex_description='test2 node', ex_network=network,
+                                       ex_is_started=False)
+        self.assertEqual(node.id, 'e75ead52-692f-4314-8725-c8a4f4d13a87')
+        self.assertEqual(node.extra['status'].action, 'DEPLOY_SERVER')
+
     def test_create_node_response_network_domain(self):
         rootPw = NodeAuthPassword('pass123')
         location = self.driver.ex_get_location_by_id('NA9')
         image = self.driver.list_images(location=location)[0]
         network_domain = self.driver.ex_list_network_domains(location=location)[0]
         vlan = self.driver.ex_list_vlans(location=location)[0]
+        cpu = DimensionDataServerCpuSpecification(
+            cpu_count=4,
+            cores_per_socket=1,
+            performance='HIGHPERFORMANCE'
+        )
+        node = self.driver.create_node(name='test2', image=image, auth=rootPw,
+                                       ex_description='test2 node',
+                                       ex_network_domain=network_domain,
+                                       ex_vlan=vlan,
+                                       ex_is_started=False, ex_cpu_specification=cpu,
+                                       ex_memory_gb=4)
+        self.assertEqual(node.id, 'e75ead52-692f-4314-8725-c8a4f4d13a87')
+        self.assertEqual(node.extra['status'].action, 'DEPLOY_SERVER')
+
+    def test_create_node_response_network_domain_STR(self):
+        rootPw = NodeAuthPassword('pass123')
+        location = self.driver.ex_get_location_by_id('NA9')
+        image = self.driver.list_images(location=location)[0]
+        network_domain = self.driver.ex_list_network_domains(location=location)[0].id
+        vlan = self.driver.ex_list_vlans(location=location)[0].id
         cpu = DimensionDataServerCpuSpecification(
             cpu_count=4,
             cores_per_socket=1,
