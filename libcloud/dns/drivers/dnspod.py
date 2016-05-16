9b51377465f84b91f41452399a48bc7d8c38023a
oltjano <oltjano13@gmail.com>
2016-05-15 18:07:13 -0700

    removes unused import statements such as import urllib

--- libcloud/dns/drivers/dnspod.py
+++ libcloud/dns/drivers/dnspod.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import sys
-import urllib
 
 
 from libcloud.dns.types import Provider, ZoneDoesNotExistError, \
