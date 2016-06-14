c1194cba9fcb2d2d148f2a68fa46d74248e1e5b7
Tomaz Muraus <tomaz@tomaz.me>
2016-06-14 19:11:19 +0200

    Drop the if since we want this path to have precedence over others. Closes #812

--- libcloud/security.py
+++ libcloud/security.py
@@ -76,9 +76,7 @@ else:
 
 if has_certifi and USE_CERTIFI:
     certifi_ca_bundle_path = certifi.where()
-
-    if certifi_ca_bundle_path not in CA_CERTS_PATH:
-        CA_CERTS_PATH.insert(0, certifi_ca_bundle_path)
+    CA_CERTS_PATH.insert(0, certifi_ca_bundle_path)
 
 # Allow user to explicitly specify which CA bundle to use, using an environment
 # variable
