b5ca5aa25ad2793b49c0e68b25d0ebb965fbb408
netgenius <wuheng09@otcaix.iscas.ac.cn>
2016-06-16 22:22:04 +0800

    aliyun ecs: support deleting security group

--- libcloud/test/compute/test_ecs.py
+++ libcloud/test/compute/test_ecs.py
@@ -903,10 +903,24 @@ class ECSMockHttp(MockHttpTestCase):
         self.assertUrlContainsQueryParams(url, params)
         return self._DescribeSecurityGroups(method, url, body, headers)
 
+    def _create_sg_CreateSecurityGroup(self, method, url, body, headers):
+        params = {'RegionId': self.test.region,
+                  'Description': 'description',
+                  'ClientToken': 'clientToken'}
+        self.assertUrlContainsQueryParams(url, params)
+        resp_body = self.fixtures.load('create_security_group.xml')
+        return (httplib.OK, resp_body, {}, httplib.responses[httplib.OK])
+
+    def _delete_sg_by_id_DeleteSecurityGroup(self, method, url, body, headers):
+        params = {'RegionId': self.test.region,
+                  'SecurityGroupId': 'sg-fakeSecurityGroupId'}
+        self.assertUrlContainsQueryParams(url, params)
+        resp_body = self.fixtures.load('delete_security_group_by_id.xml')
+        return (httplib.OK, resp_body, {}, httplib.responses[httplib.OK])
+
     def _DescribeZones(self, method, url, body, headers):
         resp_body = self.fixtures.load('describe_zones.xml')
         return (httplib.OK, resp_body, {}, httplib.responses[httplib.OK])
-
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
