7fd739bcfe0cfa705711221bfc7e15bea281f18d
netgenius <wuheng09@otcaix.iscas.ac.cn>
2016-06-25 08:52:06 +0800

    ecs_support_list_security_group_attributes

--- libcloud/test/compute/test_ecs.py
+++ libcloud/test/compute/test_ecs.py
@@ -917,6 +917,14 @@ class ECSMockHttp(MockHttpTestCase):
         self.assertUrlContainsQueryParams(url, params)
         resp_body = self.fixtures.load('delete_security_group_by_id.xml')
         return (httplib.OK, resp_body, {}, httplib.responses[httplib.OK])
+    
+    def _list_sgas_DescribeSecurityGroupAttributes(self, method, url, body, headers):
+        params = {'RegionId': self.test.region,
+                  'SecurityGroupId': 'sg-fakeSecurityGroupId',
+                  'NicType': 'internet'}
+        self.assertUrlContainsQueryParams(url, params)
+        resp_body = self.fixtures.load('describe_security_group_attributes.xml')
+        return (httplib.OK, resp_body, {}, httplib.responses[httplib.OK])
 
     def _DescribeZones(self, method, url, body, headers):
         resp_body = self.fixtures.load('describe_zones.xml')
