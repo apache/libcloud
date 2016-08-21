cec35c5399407d6c832803bc78133fe1d1428fab
Son Pham <me@famanson.com>
2016-08-21 23:00:01 +0100

    Copy pasta the HTTP request code

--- libcloud/compute/drivers/azure_arm.py
+++ libcloud/compute/drivers/azure_arm.py
@@ -1,8 +1,15 @@
-from libcloud.common.azure import AzureResourceManagerConnection
+import sys
+
+from libcloud.common.azure import AzureResourceManagerConnection, AzureRedirectException
 from libcloud.compute.base import NodeDriver
+from libcloud.compute.drivers.azure import AzureHTTPRequest
+from libcloud.compute.drivers.vcloud import urlparse
 from libcloud.compute.types import Provider
 
+from libcloud.utils.py3 import urlquote as url_quote
+
 AZURE_RESOURCE_MANAGEMENT_HOST = 'management.azure.com'
+API_VERSION = '2016-03-30'
 
 
 class AzureARMNodeDriver(NodeDriver):
@@ -25,6 +32,62 @@ class AzureARMNodeDriver(NodeDriver):
             secure=True,
             **kwargs
         )
+
+    def _perform_get(self, path):
+        request = AzureHTTPRequest()
+        request.method = 'GET'
+        request.host = AZURE_RESOURCE_MANAGEMENT_HOST
+        request.path = path
+        request.path, request.query = self._update_request_uri_query(request)
+        return self._perform_request(request)
+
+    def _update_request_uri_query(self, request):
+        """
+        pulls the query string out of the URI and moves it into
+        the query portion of the request object.  If there are already
+        query parameters on the request the parameters in the URI will
+        appear after the existing parameters
+        """
+        if '?' in request.path:
+            request.path, _, query_string = request.path.partition('?')
+            if query_string:
+                query_params = query_string.split('&')
+                for query in query_params:
+                    if '=' in query:
+                        name, _, value = query.partition('=')
+                        request.query.append((name, value))
+
+        request.path = url_quote(request.path, '/()$=\',')
+
+        # add encoded queries to request.path.
+        if request.query:
+            request.path += '?'
+            for name, value in request.query:
+                if value is not None:
+                    request.path += '%s=%s%s' % (
+                        name,
+                        url_quote(value, '/()$=\','),
+                        '&'
+                    )
+            request.path = request.path[:-1]
+
+        return request.path, request.query
+
+    def _perform_request(self, request):
+        try:
+            return self.connection.request(
+                action=request.path,
+                data=request.body,
+                headers=request.headers,
+                method=request.method
+            )
+        except AzureRedirectException:
+            e = sys.exc_info()[1]
+            parsed_url = urlparse.urlparse(e.location)
+            request.host = parsed_url.netloc
+            return self._perform_request(request)
+        except Exception as e:
+            raise e
 
     def _ex_connection_class_kwargs(self):
         """
