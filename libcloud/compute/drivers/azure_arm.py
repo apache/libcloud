7131c77f738a2e4ea614754d8372547c2227704b
Son Pham <me@famanson.com>
2016-08-21 22:43:22 +0100

    Refactor machine types and initialiser for the new driver

--- libcloud/compute/drivers/azure_arm.py
+++ libcloud/compute/drivers/azure_arm.py
@@ -1,5 +1,34 @@
+from libcloud.common.azure import AzureResourceManagerConnection
 from libcloud.compute.base import NodeDriver
+from libcloud.compute.types import Provider
 
 
 class AzureARMNodeDriver(NodeDriver):
-    pass
+    connectionCls = AzureResourceManagerConnection
+    name = 'Azure Virtual machines'
+    website = 'http://azure.microsoft.com/en-us/services/virtual-machines/'
+    type = Provider.AZURE_ARM
+
+    def __init__(self, subscription_id=None, token=None, **kwargs):
+        """
+        subscription_id contains the Azure subscription id in the form of GUID
+        key_file contains the Azure X509 certificate in .pem form
+        """
+        self.subscription_id = subscription_id
+        self.token = token
+        self.follow_redirects = kwargs.get('follow_redirects', True)
+        super(AzureARMNodeDriver, self).__init__(
+            self.subscription_id,
+            self.token,
+            secure=True,
+            **kwargs
+        )
+
+    def _ex_connection_class_kwargs(self):
+        """
+        Return extra connection keyword arguments which are passed to the
+        Connection class constructor.
+        """
+        return {
+            'token': self.token
+        }
