5c6cc2f36f11ca78a50ee357c5b58acc2fdb0183
Fahri Cihan Demirci <femnad@users.noreply.github.com>
2016-10-16 07:16:41 -0400

    [LIBCLOUD-862] Implement SSH Key Create/Delete Methods for Vultr
    * Add methods to the Vultr compute driver for creating and deleting SSH key
      pairs.
    * Add the corresponding tests for the Vultr compute driver test suite.

--- libcloud/test/compute/test_vultr.py
+++ libcloud/test/compute/test_vultr.py
@@ -84,6 +84,23 @@ class VultrTests(LibcloudTestCase):
         result = self.driver.destroy_node(node)
         self.assertTrue(result)
 
+    def test_list_key_pairs_success(self):
+        key_pairs = self.driver.list_key_pairs()
+        self.assertEqual(len(key_pairs), 1)
+        key_pair = key_pairs[0]
+        self.assertEqual(key_pair.id, '5806a8ef2a0c6')
+        self.assertEqual(key_pair.name, 'test-key-pair')
+
+    def test_create_key_pair_success(self):
+        res = self.driver.create_key_pair('test-key-pair')
+        self.assertTrue(res)
+
+    def test_delete_key_pair_success(self):
+        key_pairs = self.driver.list_key_pairs()
+        key_pair = key_pairs[0]
+        res = self.driver.delete_key_pair(key_pair)
+        self.assertTrue(res)
+
 
 class VultrMockHttp(MockHttpTestCase):
     fixtures = ComputeFileFixtures('vultr')
@@ -109,6 +126,17 @@ class VultrMockHttp(MockHttpTestCase):
 
     def _v1_server_reboot(self, method, url, body, headers):
         return (httplib.OK, "", {}, httplib.responses[httplib.OK])
+
+    def _v1_sshkey_list(self, method, url, body, headers):
+        body = self.fixtures.load('list_key_pairs.json')
+        return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+
+    def _v1_sshkey_create(self, method, url, body, headers):
+        body = self.fixtures.load('create_key_pair.json')
+        return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+
+    def _v1_sshkey_destroy(self, method, url, body, headers):
+        return (httplib.OK, '', {}, httplib.responses[httplib.OK])
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
