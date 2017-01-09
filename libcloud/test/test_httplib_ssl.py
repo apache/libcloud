bd404be03672ed5a7a0bddb242aa92f597fab957
Anthony Shaw <anthonyshaw@apache.org>
2017-01-09 14:31:04 +1100

    remove certifi test

--- libcloud/test/test_httplib_ssl.py
+++ libcloud/test/test_httplib_ssl.py
@@ -84,52 +84,5 @@ class TestHttpLibSSLTests(unittest.TestCase):
 
         self.assertTrue(self.httplib_object.ca_cert is not None)
 
-    def test_certifi_ca_bundle_in_search_path(self):
-        mock_certifi_ca_bundle_path = '/certifi/bundle/path'
-
-        # Certifi not available
-        import libcloud.security
-        reload(libcloud.security)
-
-        original_length = len(libcloud.security.CA_CERTS_PATH)
-
-        self.assertTrue(mock_certifi_ca_bundle_path not in
-                        libcloud.security.CA_CERTS_PATH)
-
-        # Certifi is available
-        mock_certifi = mock.Mock()
-        mock_certifi.where.return_value = mock_certifi_ca_bundle_path
-        sys.modules['certifi'] = mock_certifi
-
-        # Certifi CA bundle path should be injected at the begining of search list
-        import libcloud.security
-        reload(libcloud.security)
-
-        self.assertEqual(libcloud.security.CA_CERTS_PATH[0],
-                         mock_certifi_ca_bundle_path)
-        self.assertEqual(len(libcloud.security.CA_CERTS_PATH),
-                         (original_length + 1))
-
-        # Certifi is available, but USE_CERTIFI is set to False
-        os.environ['LIBCLOUD_SSL_USE_CERTIFI'] = 'false'
-
-        import libcloud.security
-        reload(libcloud.security)
-
-        self.assertTrue(mock_certifi_ca_bundle_path not in
-                        libcloud.security.CA_CERTS_PATH)
-        self.assertEqual(len(libcloud.security.CA_CERTS_PATH), original_length)
-
-        # And enabled
-        os.environ['LIBCLOUD_SSL_USE_CERTIFI'] = 'true'
-
-        import libcloud.security
-        reload(libcloud.security)
-
-        self.assertEqual(libcloud.security.CA_CERTS_PATH[0],
-                         mock_certifi_ca_bundle_path)
-        self.assertEqual(len(libcloud.security.CA_CERTS_PATH),
-                         (original_length + 1))
-
 if __name__ == '__main__':
     sys.exit(unittest.main())
