2d2374c49edbc6b8373fb230f4c08a771aa1390c
Francisco Ros <fjros@doalitic.com>
2017-05-09 12:08:56 +0200

    Fix api name for eu-west-2 region in scrape-ec2-prices.py

--- contrib/scrape-ec2-prices.py
+++ contrib/scrape-ec2-prices.py
@@ -123,7 +123,7 @@ REGION_NAME_MAP = {
     'us-west-1': 'ec2_us_west',
     'us-west-2': 'ec2_us_west_oregon',
     'eu-west-1': 'ec2_eu_west',
-    'eu-west-2': 'ec2_eu_west_2',
+    'eu-west-2': 'ec2_eu_west_london',
     'eu-ireland': 'ec2_eu_west',
     'eu-central-1': 'ec2_eu_central',
     'ca-central-1': 'ec2_ca_central',
