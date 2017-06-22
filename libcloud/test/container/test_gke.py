a1e0068acc59cb6f55c38407190d950066dc4dcb
andy <andymaheshw@gmail.com>
2017-06-21 21:45:35 -0400

    update for test

--- libcloud/test/container/test_gke.py
+++ libcloud/test/container/test_gke.py
@@ -51,8 +51,8 @@ class GKEContainerDriverTestCase(GoogleTestCase, TestCaseMixin):
 
     def test_list_images_response(self):
         config = self.driver.list_clusters(ex_zone="us-central1-a")
-        assert "zone" in config
-        assert config.zone == "us-central1-a"
+        assert "clusters" in config
+        assert config["clusters"][0]["zone"] == "us-central1-a"
 
     def test_server_config(self):
         config = self.driver.get_server_config()
