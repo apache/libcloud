c1c0a7526b90141ec09becc34e3522bf1cbdc24b
jhelie <jean.helie@gmail.com>
2017-06-22 13:52:41 +0100

    remove unecessary pass statements

--- libcloud/compute/drivers/oneandone.py
+++ libcloud/compute/drivers/oneandone.py
@@ -2249,7 +2249,6 @@ class OneAndOneNodeDriver(NodeDriver):
             if i == retries:
                 raise Exception('Retries count reached')
 
-        pass
 
     def _list_fixed_instances(self):
         response = self.connection.request(
