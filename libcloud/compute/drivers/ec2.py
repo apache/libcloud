36826f00f6be5af6e08c53abb84d4a502125c389
Lucas Di Pentima <ldipentima@veritasgenetics.com>
2017-08-30 18:03:47 -0300

    Updated code so it's Python 3 compatible.

--- libcloud/compute/drivers/ec2.py
+++ libcloud/compute/drivers/ec2.py
@@ -3772,7 +3772,7 @@ class BaseEC2NodeDriver(NodeDriver):
         tagspec_root = 'TagSpecification.1.'
         params[tagspec_root + 'ResourceType'] = 'instance'
         tag_nr = 1
-        for k, v in tags.iteritems():
+        for k, v in tags.items():
             tag_root = tagspec_root + "Tag.{}.".format(tag_nr)
             params[tag_root + "Key"] = k
             params[tag_root + "Value"] = v
