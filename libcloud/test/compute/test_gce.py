eabf95d071afcc74d6078b2844257b56596e77a0
Evan Carter <evan.carter@iextrading.com>
2017-09-28 12:47:57 -0400

    typo

--- libcloud/test/compute/test_gce.py
+++ libcloud/test/compute/test_gce.py
@@ -215,7 +215,7 @@ class GCENodeDriverTest(GoogleTestCase, TestCaseMixin):
         self.assertTrue('scopes' in actual)
 
         input = {'scopes': ['compute-ro'], 'email': 'test@test.com'}
-        actual = self.driver._build_service_account_with_email_gce_struct(input)
+        actual = self.driver._build_service_account_gce_struct(input)
         self.assertTrue('email' in actual)
         self.assertEqual(actual['email'], 'test@test.com')
         self.assertTrue('scopes' in actual)
