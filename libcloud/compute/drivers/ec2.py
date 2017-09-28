51999ff857d340746c664e92775bfa6d5a33dd7f
dgaidukov <d.gaidukov@scalr.com>
2017-09-28 16:23:25 +0300

    SCALRCORE-7105 cn-north-1 added to allowed regions

--- libcloud/compute/drivers/ec2.py
+++ libcloud/compute/drivers/ec2.py
@@ -3182,7 +3182,7 @@ VOLUME_MODIFICATION_ATTRIBUTE_MAP = {
 
 VALID_EC2_REGIONS = REGION_DETAILS.keys()
 VALID_EC2_REGIONS = [
-    r for r in VALID_EC2_REGIONS if r != 'nimbus' and r != 'cn-north-1'
+        r for r in VALID_EC2_REGIONS if r != 'nimbus' # and r != 'cn-north-1'  #  reason: SCALRCORE-7105
 ]
 VALID_VOLUME_TYPES = ['standard', 'io1', 'gp2', 'st1', 'sc1']
 
