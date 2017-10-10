3f1d9a78d4d7d5362205742973677055a74f1818
Daniel Hunsaker <danhunsaker@gmail.com>
2017-10-09 19:49:52 -0600

    [scaleway] Update sizes; add location support
    Scaleway have added additional sizes, and there is additional information about them we can report in `extra` which can be useful for users (such as the architecture of the CPUs).
    
    Also add location support, as they've apparently increased their number of locations since the original PR as well.

--- libcloud/compute/drivers/scaleway.py
+++ libcloud/compute/drivers/scaleway.py
@@ -23,7 +23,7 @@ except:
     import json
 
 from libcloud.common.base import ConnectionUserAndKey, JsonResponse
-from libcloud.compute.base import NodeDriver, NodeImage, Node, NodeSize
+from libcloud.compute.base import NodeDriver, NodeImage, Node, NodeSize, NodeLocation
 from libcloud.compute.base import StorageVolume, VolumeSnapshot
 from libcloud.compute.providers import Provider
 from libcloud.compute.types import NodeState, VolumeSnapshotState
@@ -36,45 +36,53 @@ __all__ = [
     'ScalewayNodeDriver'
 ]
 
-# 'api.scaleway.com' works only with this
-# libcloud.security.VERIFY_SSL_CERT = False
-SCALEWAY_API_HOST = 'api.cloud.online.net'
+SCALEWAY_API_HOST = 'api.scaleway.com'
 
 # The API doesn't currently expose all of the required values for libcloud,
 # so we simply list what's available right now, along with all of the various
 # attributes that are needed by libcloud.
 SCALEWAY_INSTANCE_TYPES = [
     {
-        'id': 'C1',
-        'name': 'C1',
+        'id': 'ARM64-2GB',
+        'name': 'ARM64-2GB',
         'ram': 2048,
         'disk': 50,
         'bandwidth': 200,
-        'price': 0.006
+        'price': 0.006,
+        'extra': {
+            'cores': 4,
+            'monthly': 2.99,
+            'range': 'Starter',
+            'arch': 'arm',
+        },
     },
     {
-        'id': 'C2S',
-        'name': 'C2S',
+        'id': 'ARM64-4GB',
+        'name': 'ARM64-4GB',
+        'ram': 4096,
+        'disk': 50,
+        'bandwidth': 200,
+        'price': 0.012,
+        'extra': {
+            'cores': 6,
+            'monthly': 5.99,
+            'range': 'Starter',
+            'arch': 'arm',
+        },
+    },
+    {
+        'id': 'ARM64-8GB',
+        'name': 'ARM64-8GB',
         'ram': 8192,
         'disk': 50,
-        'bandwidth': 300,
-        'price': 0.024
-    },
-    {
-        'id': 'C2M',
-        'name': 'C2M',
-        'ram': 16384,
-        'disk': 50,
-        'bandwidth': 500,
-        'price': 0.036
-    },
-    {
-        'id': 'C2L',
-        'name': 'C2L',
-        'ram': 32768,
-        'disk': 50,
-        'bandwidth': 800,
-        'price': 0.048
+        'bandwidth': 200,
+        'price': 0.024,
+        'extra': {
+            'cores': 8,
+            'monthly': 11.99,
+            'range': 'Starter',
+            'arch': 'arm',
+        },
     },
     {
         'id': 'VC1S',
@@ -82,25 +90,217 @@ SCALEWAY_INSTANCE_TYPES = [
         'ram': 2048,
         'disk': 50,
         'bandwidth': 200,
-        'price': 0.006
+        'price': 0.006,
+        'extra': {
+            'cores': 2,
+            'monthly': 2.99,
+            'range': 'Starter',
+            'arch': 'x86_64',
+        },
     },
     {
         'id': 'VC1M',
         'name': 'VC1M',
         'ram': 4096,
-        'disk': 100,
+        'disk': 50,
         'bandwidth': 200,
-        'price': 0.012},
+        'price': 0.012,
+        'extra': {
+            'cores': 4,
+            'monthly': 5.99,
+            'range': 'Starter',
+            'arch': 'x86_64',
+        },
+    },
     {
         'id': 'VC1L',
         'name': 'VC1L',
         'ram': 8192,
-        'disk': 200,
+        'disk': 50,
         'bandwidth': 200,
-        'price': 0.02
+        'price': 0.02,
+        'extra': {
+            'cores': 6,
+            'monthly': 9.99,
+            'range': 'Starter',
+            'arch': 'x86_64',
+        },
+    },
+    {
+        'id': 'C1',
+        'name': 'C1',
+        'ram': 2048,
+        'disk': 50,
+        'bandwidth': 200,
+        'price': 0.006,
+        'extra': {
+            'cores': 4,
+            'monthly': 2.99,
+            'range': 'Baremetal',
+            'arch': 'arm',
+        },
+    },
+    {
+        'id': 'C2S',
+        'name': 'C2S',
+        'ram': 8192,
+        'disk': 50,
+        'bandwidth': 300,
+        'price': 0.024,
+        'extra': {
+            'cores': 4,
+            'monthly': 11.99,
+            'range': 'Baremetal',
+            'arch': 'x86_64',
+        },
+    },
+    {
+        'id': 'C2M',
+        'name': 'C2M',
+        'ram': 16384,
+        'disk': 50,
+        'bandwidth': 500,
+        'price': 0.036,
+        'extra': {
+            'cores': 8,
+            'monthly': 17.99,
+            'range': 'Baremetal',
+            'arch': 'x86_64',
+        },
+    },
+    {
+        'id': 'C2L',
+        'name': 'C2L',
+        'ram': 32768,
+        'disk': 50,
+        'bandwidth': 800,
+        'price': 0.048,
+        'extra': {
+            'cores': 8,
+            'monthly': 23.99,
+            'range': 'Baremetal',
+            'arch': 'x86_64',
+        },
+    },
+    {
+        'id': 'ARM64-16GB',
+        'name': 'ARM64-16GB',
+        'ram': 16384,
+        'disk': 50,
+        'bandwidth': 500,
+        'price': 0.07,
+        'extra': {
+            'cores': 16,
+            'monthly': 34.99,
+            'range': 'Intensive',
+            'arch': 'arm',
+        },
+    },
+    {
+        'id': 'ARM64-32GB',
+        'name': 'ARM64-32GB',
+        'ram': 32768,
+        'disk': 50,
+        'bandwidth': 500,
+        'price': 0.14,
+        'extra': {
+            'cores': 32,
+            'monthly': 69.99,
+            'range': 'Intensive',
+            'arch': 'arm',
+        },
+    },
+    {
+        'id': 'ARM64-64GB',
+        'name': 'ARM64-64GB',
+        'ram': 65536,
+        'disk': 50,
+        'bandwidth': 1000,
+        'price': 0.28,
+        'extra': {
+            'cores': 48,
+            'monthly': 139.99,
+            'range': 'Intensive',
+            'arch': 'arm',
+        },
+    },
+    {
+        'id': 'ARM64-128GB',
+        'name': 'ARM64-128GB',
+        'ram': 131072,
+        'disk': 50,
+        'bandwidth': 1000,
+        'price': 0.56,
+        'extra': {
+            'cores': 64,
+            'monthly': 279.99,
+            'range': 'Intensive',
+            'arch': 'arm',
+        },
+    },
+    {
+        'id': 'X64-15GB',
+        'name': 'X64-15GB',
+        'ram': 15360,
+        'disk': 50,
+        'bandwidth': 250,
+        'price': 0.05,
+        'extra': {
+            'cores': 6,
+            'monthly': 24.99,
+            'range': 'Intensive',
+            'arch': 'x86_64',
+        },
+    },
+    {
+        'id': 'X64-30GB',
+        'name': 'X64-30GB',
+        'ram': 30720,
+        'disk': 50,
+        'bandwidth': 500,
+        'price': 0.1,
+        'extra': {
+            'cores': 8,
+            'monthly': 49.99,
+            'range': 'Intensive',
+            'arch': 'x86_64',
+        },
+    },
+    {
+        'id': 'X64-60GB',
+        'name': 'X64-60GB',
+        'ram': 61440,
+        'disk': 50,
+        'bandwidth': 1000,
+        'price': 0.18,
+        'extra': {
+            'cores': 10,
+            'monthly': 89.99,
+            'range': 'Intensive',
+            'arch': 'x86_64',
+        },
+    },
+    {
+        'id': 'X64-120GB',
+        'name': 'X64-120GB',
+        'ram': 122880,
+        'disk': 50,
+        'bandwidth': 1000,
+        'price': 0.36,
+        'extra': {
+            'cores': 12,
+            'monthly': 179.99,
+            'range': 'Intensive',
+            'arch': 'x86_64',
+        },
     },
 ]
 
+# The API also doesn't give location info, so we provide it ourselves, instead.
+SCALEWAY_LOCATION_DATA = [
+    {'id': 'par1', 'name': 'Paris 1', 'country': 'FR'},
+    {'id': 'ams1', 'name': 'Amsterdam 1', 'country': 'NL'},
+]
 
 class ScalewayResponse(JsonResponse):
     valid_response_codes = [httplib.OK, httplib.ACCEPTED,
@@ -121,6 +321,19 @@ class ScalewayConnection(ConnectionUserAndKey):
     host = SCALEWAY_API_HOST
     allow_insecure = False
     responseCls = ScalewayResponse
+
+    def request(self, action, params=None, data=None, headers=None,
+                method='GET', raw=False, stream=False, region=None):
+        if region:
+            old_host = self.host
+            self.host = 'cp-%s.scaleway.com' % (region.id if isinstance(region,
+                                                NodeLocation) else region)
+            if not self.host == old_host:
+                self.connect()
+
+        return super(ScalewayConnection, self).request(action, params, data,
+                                                       headers, method, raw,
+                                                       stream)
 
     def add_default_headers(self, headers):
         """
@@ -153,12 +366,16 @@ class ScalewayNodeDriver(NodeDriver):
         'snapshotting': VolumeSnapshotState.CREATING
     }
 
+    def list_locations(self):
+        return [NodeLocation(driver=self, **copy.deepcopy(location))
+                for location in SCALEWAY_LOCATION_DATA]
+
     def list_sizes(self):
         return [NodeSize(driver=self, **copy.deepcopy(size))
                 for size in SCALEWAY_INSTANCE_TYPES]
 
-    def list_images(self):
-        response = self.connection.request('/images')
+    def list_images(self, region=None):
+        response = self.connection.request('/images', region=region)
         images = response.object['images']
         return [NodeImage(id=image['id'],
                           name=image['name'],
@@ -166,7 +383,7 @@ class ScalewayNodeDriver(NodeDriver):
                           extra={'arch': image['arch']})
                 for image in images]
 
-    def create_image(self, node, name, description=None):
+    def create_image(self, node, name, description=None, region=None):
         data = {
             'organization': self.key,
             'name': name,
@@ -174,21 +391,24 @@ class ScalewayNodeDriver(NodeDriver):
             'root_volume': node.extra['volumes']['0']['id']  # TODO check this
         }
         response = self.connection.request('/images', data=json.dumps(data),
+                                           region=region,
                                            method='POST')
         image = response.object['image']
         return NodeImage(id=image['id'], name=image['name'], driver=self)
 
-    def delete_image(self, node_image):
+    def delete_image(self, node_image, region=None):
         return self.connection.request('/images/%s' % node_image.id,
+                                       region=region,
                                        method='DELETE').success()
 
-    def get_image(self, image_id):
-        response = self.connection.request('/images/%s' % image_id)
+    def get_image(self, image_id, region=None):
+        response = self.connection.request('/images/%s' % image_id,
+                                           region=region)
         image = response.object['image']
         return NodeImage(id=image['id'], name=image['name'], driver=self)
 
-    def list_nodes(self):
-        response = self.connection.request('/servers')
+    def list_nodes(self, region=None):
+        response = self.connection.request('/servers', region=region)
         servers = response.object['servers']
         return [self._to_node(server) for server in servers]
 
@@ -204,7 +424,8 @@ class ScalewayNodeDriver(NodeDriver):
                     extra={'volumes': server['volumes']},
                     created_at=parse_date(server['creation_date']))
 
-    def create_node(self, name, size, image, ex_volumes=None, ex_tags=None):
+    def create_node(self, name, size, image, ex_volumes=None, ex_tags=None,
+                    region=None):
         data = {
             'name': name,
             'organization': self.key,
@@ -215,6 +436,7 @@ class ScalewayNodeDriver(NodeDriver):
         }
 
         response = self.connection.request('/servers', data=json.dumps(data),
+                                           region=region,
                                            method='POST')
         server = response.object['server']
         node = self._to_node(server)
@@ -224,8 +446,9 @@ class ScalewayNodeDriver(NodeDriver):
 
         return node
 
-    def _action(self, server_id, action):
+    def _action(self, server_id, action, region=None):
         return self.connection.request('/servers/%s/action' % server_id,
+                                       region=region,
                                        data=json.dumps({'action': action}),
                                        method='POST').success()
 
@@ -235,8 +458,8 @@ class ScalewayNodeDriver(NodeDriver):
     def destroy_node(self, node):
         return self._action(node.id, 'terminate')
 
-    def list_volumes(self):
-        response = self.connection.request('/volumes')
+    def list_volumes(self, region=None):
+        response = self.connection.request('/volumes', region=region)
         volumes = response.object['volumes']
         return [self._to_volume(volume) for volume in volumes]
 
@@ -246,8 +469,8 @@ class ScalewayNodeDriver(NodeDriver):
                              size=_kb_to_mb(volume['size']),
                              driver=self)
 
-    def list_volume_snapshots(self, volume):
-        response = self.connection.request('/snapshots')
+    def list_volume_snapshots(self, volume, region=None):
+        response = self.connection.request('/snapshots', region=region)
         snapshots = filter(lambda s: s['base_volume']['id'] == volume.id,
                            response.object['snapshots'])
         return [self._to_snapshot(snapshot) for snapshot in snapshots]
@@ -261,7 +484,7 @@ class ScalewayNodeDriver(NodeDriver):
                               created=parse_date(snapshot['creation_date']),
                               state=state)
 
-    def create_volume(self, size, name):
+    def create_volume(self, size, name, region=None):
         data = {
             'name': name,
             'organization': self.key,
@@ -269,27 +492,31 @@ class ScalewayNodeDriver(NodeDriver):
             'size': _mb_to_kb(size)
         }
         response = self.connection.request('/volumes',
+                                           region=region,
                                            data=json.dumps(data),
                                            method='POST')
         volume = response.object['volume']
         return self._to_volume(volume)
 
-    def create_volume_snapshot(self, volume, name):
+    def create_volume_snapshot(self, volume, name, region=None):
         data = {
             'name': name,
             'organization': self.key,
             'volume_id': volume.id
         }
         response = self.connection.request('/snapshots',
+                                           region=region,
                                            data=json.dumps(data),
                                            method='POST')
         snapshot = response.object['snapshot']
         return self._to_snapshot(snapshot)
 
-    def destroy_volume(self, volume):
+    def destroy_volume(self, volume, region=None):
         return self.connection.request('/volumes/%s' % volume.id,
+                                       region=region,
                                        method='DELETE').success()
 
-    def destroy_volume_snapshot(self, snapshot):
+    def destroy_volume_snapshot(self, snapshot, region=None):
         return self.connection.request('/snapshots/%s' % snapshot.id,
+                                       region=region,
                                        method='DELETE').success()
