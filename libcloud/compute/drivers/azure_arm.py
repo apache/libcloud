f645a884a1f2677138e7795a67bf807c66230c47
Mike Muzurakis <clopy@mist.io>
2017-10-13 20:59:14 +0300

    better handling of windows images

--- libcloud/compute/drivers/azure_arm.py
+++ libcloud/compute/drivers/azure_arm.py
@@ -645,7 +645,7 @@ class AzureNodeDriver(NodeDriver):
         if isinstance(auth, NodeAuthSSHKey):
             data["properties"]["osProfile"]["adminPassword"] = \
                 binascii.hexlify(os.urandom(20))
-            if not "Windows" in image.id:
+            if not "windows" in image.id.lower():
                 data["properties"]["osProfile"]["linuxConfiguration"] = {
                     "disablePasswordAuthentication": "true",
                     "ssh": {
@@ -659,7 +659,7 @@ class AzureNodeDriver(NodeDriver):
                     }
                 }
         elif isinstance(auth, NodeAuthPassword):
-            if not "Windows" in image.id:
+            if not "Windows" in image.id.lower():
                 data["properties"]["osProfile"]["linuxConfiguration"] = {
                     "disablePasswordAuthentication": "false"
                 }
