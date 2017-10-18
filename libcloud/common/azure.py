c4baa8b18d6c744873a3602cb0f5614a5f6fc2db
Peter Amstutz <pamstutz@veritasgenetics.com>
2017-10-18 20:35:59 +0000

    No content-length on HEAD either

--- libcloud/common/azure.py
+++ libcloud/common/azure.py
@@ -193,8 +193,8 @@ class AzureConnection(ConnectionUserAndKey):
             header = header.lower()  # Just for safety
             if header in headers_copy:
                 special_header_values.append(headers_copy[header])
-            elif header == "content-length" and method != "GET":
-                # Must be '0' unless method is GET
+            elif header == "content-length" and method not in ("GET", "HEAD"):
+                # Must be '0' unless method is GET or HEAD
                 # https://docs.microsoft.com/en-us/rest/api/storageservices/authentication-for-the-azure-storage-services
                 special_header_values.append('0')
             else:
