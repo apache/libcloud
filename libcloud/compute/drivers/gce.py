69d601d0f79d261fcb0ae5f50f1b6b6ad46c6e55
Gareth <31602864+vongazman@users.noreply.github.com>
2017-10-27 16:26:52 +1100

    Fixed _create_node_req paramaters

--- libcloud/compute/drivers/gce.py
+++ libcloud/compute/drivers/gce.py
@@ -3915,11 +3915,11 @@ class GCENodeDriver(NodeDriver):
 
         request, node_data = self._create_node_req(
             name, size, image, location, ex_network, ex_tags, ex_metadata,
-            ex_boot_disk, external_ip, ex_disk_type, ex_disk_auto_delete,
-            ex_service_accounts, description, ex_can_ip_forward,
-            ex_disks_gce_struct, ex_nic_gce_struct, ex_on_host_maintenance,
-            ex_automatic_restart, ex_preemptible, ex_subnetwork, ex_labels,
-            internal_ip)
+            ex_boot_disk, external_ip, internal_ip, ex_disk_type,
+            ex_disk_auto_delete, ex_service_accounts, description,
+            ex_can_ip_forward, ex_disks_gce_struct, ex_nic_gce_struct,
+            ex_on_host_maintenance,ex_automatic_restart, ex_preemptible,
+            ex_subnetwork, ex_labels)
         self.connection.async_request(request, method='POST', data=node_data)
         return self.ex_get_node(name, location.name)
 
