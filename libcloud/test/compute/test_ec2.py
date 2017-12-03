aeef1cc05a2920defb56fe63470262016f46bdf6
Anthony Monthe <anthony.monthe@gmail.com>
2017-12-03 00:44:48 +0000

    Added AWS EC2 c5 series

--- libcloud/test/compute/test_ec2.py
+++ libcloud/test/compute/test_ec2.py
@@ -448,13 +448,13 @@ class EC2Tests(LibcloudTestCase, TestCaseMixin):
                 self.assertTrue('m2.4xlarge' in ids)
 
             if region_name == 'us-east-1':
-                self.assertEqual(len(sizes), 72)
+                self.assertEqual(len(sizes), 78)
                 self.assertTrue('cg1.4xlarge' in ids)
                 self.assertTrue('cc2.8xlarge' in ids)
                 self.assertTrue('cr1.8xlarge' in ids)
                 self.assertTrue('x1.32xlarge' in ids)
             elif region_name == 'us-west-1':
-                self.assertEqual(len(sizes), 61)
+                self.assertEqual(len(sizes), 67)
             if region_name == 'us-west-2':
                 self.assertEqual(len(sizes), 73)
             elif region_name == 'ap-southeast-1':
@@ -462,7 +462,7 @@ class EC2Tests(LibcloudTestCase, TestCaseMixin):
             elif region_name == 'ap-southeast-2':
                 self.assertEqual(len(sizes), 63)
             elif region_name == 'eu-west-1':
-                self.assertEqual(len(sizes), 70)
+                self.assertEqual(len(sizes), 76)
             elif region_name == 'ap-south-1':
                 self.assertEqual(len(sizes), 41)
 
