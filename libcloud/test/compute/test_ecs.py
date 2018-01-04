3ea4a343c9ef6d69ec5a8d79e74db44776cc9407
Zhang Yiming <zhangyiming215@mails.ucas.ac.cn>
2018-01-04 06:09:00 +0000

    fix lint error in test_ecs.py

--- libcloud/test/compute/test_ecs.py
+++ libcloud/test/compute/test_ecs.py
@@ -942,7 +942,7 @@ class ECSMockHttp(MockHttp):
         self.assertUrlContainsQueryParams(url, params)
         resp_body = self.fixtures.load('delete_security_group_by_id.xml')
         return (httplib.OK, resp_body, {}, httplib.responses[httplib.OK])
-        
+
     def _modify_sg_by_id_ModifySecurityGroup(self, method, url, body, headers):
         params = {'RegionId': self.test.region,
                   'SecurityGroupId': 'sg-fakeSecurityGroupId',
