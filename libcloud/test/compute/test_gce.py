fca9f833deffb3c22048063efa610221421976fb
Boris Chazalet <bchazalet@companywatch.net>
2018-03-09 11:26:44 +0100

    Add a test that would fail without the fix
    Signed-off-by: Quentin Pradet <quentinp@apache.org>

--- libcloud/test/compute/test_gce.py
+++ libcloud/test/compute/test_gce.py
@@ -462,7 +462,7 @@ class GCENodeDriverTest(GoogleTestCase, TestCaseMixin):
         self.assertEqual(mig.zone.name, zone)
 
     def test_ex_create_instancegroupmanager_shared_network(self):
-        name = 'myinstancegroup'
+        name = 'myinstancegroup-shared-network'
         zone = 'us-central1-a'
         size = 4
         template_name = 'my-instance-template-shared-network'
@@ -2707,10 +2707,20 @@ class GCEMockHttp(MockHttp):
         body = self.fixtures.load('projects_other_name_global_networks_cf.json')
         return (httplib.OK, body, self.json_hdr, httplib.responses[httplib.OK])
 
+    def _projects_other_name_global_networks_shared_network_for_mig(self, method, url, body, headers):
+        body = self.fixtures.load('projects_other_name_global_networks_shared_network_for_mig.json')
+        return (httplib.OK, body, self.json_hdr, httplib.responses[httplib.OK])
+
     def _projects_other_name_regions_us_central1_subnetworks_cf_972cf02e6ad49114(
             self, method, url, body, headers):
         body = self.fixtures.load(
             'projects_other_name_regions_us-central1_subnetworks_cf_972cf02e6ad49114.json')
+        return (httplib.OK, body, self.json_hdr, httplib.responses[httplib.OK])
+
+    def _projects_other_name_regions_us_central1_subnetworks_shared_subnetwork_for_mig(
+            self, method, url, body, headers):
+        body = self.fixtures.load(
+            'projects_other_name_regions_us-central1_subnetworks_shared_subnetwork_for_mig.json')
         return (httplib.OK, body, self.json_hdr, httplib.responses[httplib.OK])
 
     def _regions_us_central1_operations_operation_regions_us_central1_addresses_lcaddress_delete(
@@ -3497,10 +3507,22 @@ class GCEMockHttp(MockHttp):
             'zones_us-east1-b_instanceGroup_myinstancegroup.json')
         return (httplib.OK, body, self.json_hdr, httplib.responses[httplib.OK])
 
+    def _zones_us_central1_a_instanceGroups_myinstancegroup_shared_network(
+            self, method, url, body, headers):
+        body = self.fixtures.load(
+            'zones_us-central1-a_instanceGroup_myinstancegroup_shared_network.json')
+        return (httplib.OK, body, self.json_hdr, httplib.responses[httplib.OK])
+
     def _zones_us_central1_a_instanceGroupManagers_myinstancegroup(
             self, method, url, body, headers):
         body = self.fixtures.load(
             'zones_us-central1-a_instanceGroupManagers_myinstancegroup.json')
+        return (httplib.OK, body, self.json_hdr, httplib.responses[httplib.OK])
+
+    def _zones_us_central1_a_instanceGroupManagers_myinstancegroup_shared_network(
+            self, method, url, body, headers):
+        body = self.fixtures.load(
+            'zones_us-central1-a_instanceGroupManagers_myinstancegroup_shared_network.json')
         return (httplib.OK, body, self.json_hdr, httplib.responses[httplib.OK])
 
     def _zones_us_central1_b_instanceGroupManagers_myinstancegroup(
