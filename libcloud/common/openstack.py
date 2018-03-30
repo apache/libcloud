7f9a71edf6626ccde4ddc893fe8da1cc5d898537
Dimitris Moraitis <dimo@mist.io>
2018-03-30 22:53:50 +0300

    Defensive error handling

--- libcloud/common/openstack.py
+++ libcloud/common/openstack.py
@@ -389,7 +389,8 @@ class OpenStackResponse(Response):
             context = self.connection.context
             driver = self.connection.driver
             key_pair_name = context.get('key_pair_name', None)
-            if len(values) > 0 and values[0].get('code') == 404 and key_pair_name:
+            if len(values) > 0 and hasattr(values[0], 'get') and \
+                values[0].get('code') == 404 and key_pair_name:
                 raise KeyPairDoesNotExistError(name=key_pair_name,
                                                driver=driver)
             elif len(values) > 0 and 'message' in values[0]:
