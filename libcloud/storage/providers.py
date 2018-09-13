358b9fa8ad124a088bb1f8a0a97012c78aeac3b7
yangkang <yangkang@gagogroup.com>
2018-09-13 23:34:08 +0800

    feat: add support about region of '''cn-northwest-1'

--- libcloud/storage/providers.py
+++ libcloud/storage/providers.py
@@ -13,77 +13,79 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from libcloud.storage.types import Provider
-from libcloud.storage.types import OLD_CONSTANT_TO_NEW_MAPPING
 from libcloud.common.providers import get_driver as _get_provider_driver
 from libcloud.common.providers import set_driver as _set_provider_driver
+from libcloud.storage.types import OLD_CONSTANT_TO_NEW_MAPPING
+from libcloud.storage.types import Provider
 
 DRIVERS = {
     Provider.DUMMY:
-    ('libcloud.storage.drivers.dummy', 'DummyStorageDriver'),
+        ('libcloud.storage.drivers.dummy', 'DummyStorageDriver'),
     Provider.CLOUDFILES:
-    ('libcloud.storage.drivers.cloudfiles', 'CloudFilesStorageDriver'),
+        ('libcloud.storage.drivers.cloudfiles', 'CloudFilesStorageDriver'),
     Provider.OPENSTACK_SWIFT:
-    ('libcloud.storage.drivers.cloudfiles', 'OpenStackSwiftStorageDriver'),
+        ('libcloud.storage.drivers.cloudfiles', 'OpenStackSwiftStorageDriver'),
     Provider.S3:
-    ('libcloud.storage.drivers.s3', 'S3StorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3StorageDriver'),
     Provider.S3_US_EAST2:
-    ('libcloud.storage.drivers.s3', 'S3USEast2StorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3USEast2StorageDriver'),
     Provider.S3_US_WEST:
-    ('libcloud.storage.drivers.s3', 'S3USWestStorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3USWestStorageDriver'),
     Provider.S3_US_WEST_OREGON:
-    ('libcloud.storage.drivers.s3', 'S3USWestOregonStorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3USWestOregonStorageDriver'),
     Provider.S3_US_GOV_WEST:
-    ('libcloud.storage.drivers.s3', 'S3USGovWestStorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3USGovWestStorageDriver'),
     Provider.S3_CN_NORTH:
-    ('libcloud.storage.drivers.s3', 'S3CNNorthStorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3CNNorthStorageDriver'),
+    Provider.S3_CN_NORTHQWEST:
+        ('libcloud.storage.drivers.s3', 'S3CNNorthWestStorageDriver'),
     Provider.S3_EU_WEST:
-    ('libcloud.storage.drivers.s3', 'S3EUWestStorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3EUWestStorageDriver'),
     Provider.S3_EU_WEST2:
-    ('libcloud.storage.drivers.s3', 'S3EUWest2StorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3EUWest2StorageDriver'),
     Provider.S3_EU_CENTRAL:
-    ('libcloud.storage.drivers.s3', 'S3EUCentralStorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3EUCentralStorageDriver'),
     Provider.S3_AP_SOUTH:
-    ('libcloud.storage.drivers.s3', 'S3APSouthStorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3APSouthStorageDriver'),
     Provider.S3_AP_SOUTHEAST:
-    ('libcloud.storage.drivers.s3', 'S3APSEStorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3APSEStorageDriver'),
     Provider.S3_AP_SOUTHEAST2:
-    ('libcloud.storage.drivers.s3', 'S3APSE2StorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3APSE2StorageDriver'),
     Provider.S3_AP_NORTHEAST:
-    ('libcloud.storage.drivers.s3', 'S3APNE1StorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3APNE1StorageDriver'),
     Provider.S3_AP_NORTHEAST1:
-    ('libcloud.storage.drivers.s3', 'S3APNE1StorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3APNE1StorageDriver'),
     Provider.S3_AP_NORTHEAST2:
-    ('libcloud.storage.drivers.s3', 'S3APNE2StorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3APNE2StorageDriver'),
     Provider.S3_SA_EAST:
-    ('libcloud.storage.drivers.s3', 'S3SAEastStorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3SAEastStorageDriver'),
     Provider.S3_CA_CENTRAL:
-    ('libcloud.storage.drivers.s3', 'S3CACentralStorageDriver'),
+        ('libcloud.storage.drivers.s3', 'S3CACentralStorageDriver'),
     Provider.S3_RGW:
-    ('libcloud.storage.drivers.rgw', 'S3RGWStorageDriver'),
+        ('libcloud.storage.drivers.rgw', 'S3RGWStorageDriver'),
     Provider.S3_RGW_OUTSCALE:
-    ('libcloud.storage.drivers.rgw', 'S3RGWOutscaleStorageDriver'),
+        ('libcloud.storage.drivers.rgw', 'S3RGWOutscaleStorageDriver'),
     Provider.NINEFOLD:
-    ('libcloud.storage.drivers.ninefold', 'NinefoldStorageDriver'),
+        ('libcloud.storage.drivers.ninefold', 'NinefoldStorageDriver'),
     Provider.GOOGLE_STORAGE:
-    ('libcloud.storage.drivers.google_storage', 'GoogleStorageDriver'),
+        ('libcloud.storage.drivers.google_storage', 'GoogleStorageDriver'),
     Provider.NIMBUS:
-    ('libcloud.storage.drivers.nimbus', 'NimbusStorageDriver'),
+        ('libcloud.storage.drivers.nimbus', 'NimbusStorageDriver'),
     Provider.LOCAL:
-    ('libcloud.storage.drivers.local', 'LocalStorageDriver'),
+        ('libcloud.storage.drivers.local', 'LocalStorageDriver'),
     Provider.AZURE_BLOBS:
-    ('libcloud.storage.drivers.azure_blobs', 'AzureBlobsStorageDriver'),
+        ('libcloud.storage.drivers.azure_blobs', 'AzureBlobsStorageDriver'),
     Provider.KTUCLOUD:
-    ('libcloud.storage.drivers.ktucloud', 'KTUCloudStorageDriver'),
+        ('libcloud.storage.drivers.ktucloud', 'KTUCloudStorageDriver'),
     Provider.AURORAOBJECTS:
-    ('libcloud.storage.drivers.auroraobjects', 'AuroraObjectsStorageDriver'),
+        ('libcloud.storage.drivers.auroraobjects', 'AuroraObjectsStorageDriver'),
     Provider.BACKBLAZE_B2:
-    ('libcloud.storage.drivers.backblaze_b2', 'BackblazeB2StorageDriver'),
+        ('libcloud.storage.drivers.backblaze_b2', 'BackblazeB2StorageDriver'),
     Provider.ALIYUN_OSS:
-    ('libcloud.storage.drivers.oss', 'OSSStorageDriver'),
+        ('libcloud.storage.drivers.oss', 'OSSStorageDriver'),
     Provider.DIGITALOCEAN_SPACES:
-    ('libcloud.storage.drivers.digitalocean_spaces',
-     'DigitalOceanSpacesStorageDriver'),
+        ('libcloud.storage.drivers.digitalocean_spaces',
+         'DigitalOceanSpacesStorageDriver'),
 }
 
 
