608598821758cab79ed5e61b370ee0407f03616a
elesbom <rafael.goncalves@corp.globo.com>
2018-09-24 15:11:35 -0300

    [LIBCLOUD-1014] PEP8
    Signed-off-by: Rick van de Loo <rickvandeloo@gmail.com>

--- libcloud/test/compute/test_ec2.py
+++ libcloud/test/compute/test_ec2.py
@@ -1803,13 +1803,12 @@ class NimbusTests(EC2Tests):
         self.assertExecutedMethodCount(0)
 
     def test_create_volume_snapshot_with_tags(self):
-            vol = StorageVolume(id='vol-4282672b', name='test',
-                                state=StorageVolumeState.AVAILABLE,
-                                size=10, driver=self.driver)
-            snap = self.driver.create_volume_snapshot(
-                vol, 'Test snapshot', ex_metadata={'my_tag': 'test'})
-            self.assertDictEqual({}, snap.extra['tags'])
-
+        vol = StorageVolume(id='vol-4282672b', name='test',
+                            state=StorageVolumeState.AVAILABLE,
+                            size=10, driver=self.driver)
+        snap = self.driver.create_volume_snapshot(
+            vol, 'Test snapshot', ex_metadata={'my_tag': 'test'})
+        self.assertDictEqual({}, snap.extra['tags'])
 
 
 class EucTests(LibcloudTestCase, TestCaseMixin):
