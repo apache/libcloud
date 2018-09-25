bcbd0ca33c4ce4649e12d8289091a850bdc38ffd
micafer <micafer1@upv.es>
2018-09-25 16:38:45 +0200

    Remove print

--- libcloud/common/openstack.py
+++ libcloud/common/openstack.py
@@ -390,7 +390,7 @@ class OpenStackResponse(Response):
             context = self.connection.context
             driver = self.connection.driver
             key_pair_name = context.get('key_pair_name', None)
-            print(values)
+
             if len(values) > 0 and 'code' in values[0] and \
                     values[0]['code'] == 404 and key_pair_name:
                 raise KeyPairDoesNotExistError(name=key_pair_name,
