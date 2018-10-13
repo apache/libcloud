436c5b00eb1f7cb79d62681459d405f871b008f9
Quentin Pradet <quentinp@apache.org>
2018-10-13 10:14:54 +0400

    dist: provide sha256/sha512 hashes

---
+++ dist/hash.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import hashlib
+import sys
+
+for path in sys.argv[1:]:
+    for ext, hash in [('sha256', hashlib.sha256), ('sha512', hashlib.sha512)]:
+        written = '{}.{}'.format(path, ext)
+        with open(path, 'rb') as fin, open(written, 'w') as fout:
+            digest = hash(fin.read()).hexdigest()
+            print('{} *{}'.format(digest, path), file=fout)
