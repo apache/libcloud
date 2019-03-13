ba73c66f7602080bae6992739778340662807c91
Dimitris Moraitis <dimo@mist.io>
2019-03-13 16:00:33 +0200

    Fix Python3 compatibility for exceptions in libvirt_driver

--- libcloud/compute/drivers/libvirt_driver.py
+++ libcloud/compute/drivers/libvirt_driver.py
@@ -157,14 +157,14 @@ class LibvirtNodeDriver(NodeDriver):
         try:
             self.connection = libvirt.open(uri)
         except Exception as exc:
-            if 'Could not resolve' in exc.message:
+            if 'Could not resolve' in str(exc):
                 raise Exception("Make sure hostname is accessible")
-            if 'Connection refused' in exc.message:
+            if 'Connection refused' in str(exc):
                 raise Exception("Make sure hostname is accessible and libvirt "
                                 "is running")
-            if 'Permission denied' in exc.message:
+            if 'Permission denied' in str(exc):
                 raise Exception("Make sure ssh key and username are valid")
-            if 'End of file while reading data' in exc.message:
+            if 'End of file while reading data' in str(exc):
                 raise Exception("Make sure libvirt is running and user %s is "
                                 "authorised to connect" % user)
             raise Exception("Connection error")
