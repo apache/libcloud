4eab1fb42f58d6203a0862aa9caf304193d3442b
mpempekos <spirostza77@hotmail.com>
2019-06-20 18:07:44 +0300

    Add Accept header to use version 1.1

--- libcloud/common/maxihost.py
+++ libcloud/common/maxihost.py
@@ -40,4 +40,5 @@ class MaxihostConnection(ConnectionKey):
         """
         headers['Authorization'] = 'Bearer %s' % (self.key)
         headers['Content-Type'] = 'application/json'
+        headers['Accept']: 'application/vnd.maxihost.v1.1+json'
         return headers
