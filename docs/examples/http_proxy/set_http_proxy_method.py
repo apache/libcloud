bab2d3185c14916a727df2e4e80e892b2c5f7196
Tomaz Muraus <tomaz@tomaz.me>
2019-07-16 21:42:28 +0200

    Update documentation on using an http proxy. Add information on how to use an https proxy.

--- docs/examples/http_proxy/set_http_proxy_method.py
+++ docs/examples/http_proxy/set_http_proxy_method.py
@@ -3,9 +3,9 @@ from pprint import pprint
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 
-PROXY_URL_NO_AUTH_1 = 'http://<proxy hostname 1>:<proxy port 2>'
-PROXY_URL_NO_AUTH_2 = 'http://<proxy hostname 1>:<proxy port 2>'
-PROXY_URL_BASIC_AUTH = 'http://<user>:<pass>@<proxy hostname>:<proxy port>'
+HTTP_PROXY_URL_NO_AUTH_1 = 'http://<proxy hostname 1>:<proxy port 2>'
+HTTP_PROXY_URL_NO_AUTH_2 = 'http://<proxy hostname 1>:<proxy port 2>'
+HTTP_PROXY_URL_BASIC_AUTH = 'http://<user>:<pass>@<proxy hostname>:<proxy port>'
 
 cls = get_driver(Provider.RACKSPACE)
 driver = cls('username', 'api key', region='ord')
