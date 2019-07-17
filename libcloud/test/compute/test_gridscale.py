dabe62746d48a6871b94ff5ce7ed1dc7837703d0
Tomaz Muraus <tomaz@tomaz.me>
2019-07-17 21:54:38 +0200

    Add coverage pragmas for else branches. We don't want per-branch coverage for those else clauses since going into else would indicate a programmer error / invalid test aka should never happen.

--- libcloud/test/compute/test_gridscale.py
+++ libcloud/test/compute/test_gridscale.py
@@ -229,32 +229,32 @@ class GridscaleMockHttp(MockHttp):
         # test_ex_rename_node
         if method == 'PATCH':
             return (httplib.NO_CONTENT, None, {}, httplib.responses[httplib.NO_CONTENT])
-
-        raise ValueError('Invalid method')
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
     def _objects_storages_e66bb753_4a03_4ee2_a069_a601f393c9ee(
         self, method, url, body, headers):
         # test_ex_rename_node
         if method == 'PATCH':
             return (httplib.NO_CONTENT, None, {}, httplib.responses[httplib.NO_CONTENT])
-
-        raise ValueError('Invalid method')
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
     def _objects_networks_1196529b_a8de_417f(
         self, method, url, body, headers):
         # test_ex_rename_network
         if method == 'PATCH':
             return (httplib.NO_CONTENT, None, {}, httplib.responses[httplib.NO_CONTENT])
-
-        raise ValueError('Invalid method')
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
     def _objects_servers_POST(self, method, url, body, headers):
         # create_node
         if method == 'POST':
             body = self.fixtures.load('create_node.json')
             return (httplib.OK, body, {}, httplib.responses[httplib.OK])
-
-        raise ValueError('Invalid method')
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
     def _requests_x123xx1x_123x_1x12_123x_123xxx123x1x_POST(self, method, url,
                                                             body, headers):
@@ -281,6 +281,8 @@ class GridscaleMockHttp(MockHttp):
         if method == 'POST':
             body = self.fixtures.load('create_ip.json')
             return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
     def _objects_ips_690de890_13c0_4e76_8a01_e10ba8786e53_POST(self, method, url,
                                                                 body, headers):
@@ -303,8 +305,8 @@ class GridscaleMockHttp(MockHttp):
         # test_ex_destroy_network
         if method == 'DELETE':
             return (httplib.NO_CONTENT, None, {}, httplib.responses[httplib.NO_CONTENT])
-
-        raise ValueError('Invalid method')
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
     def _objects_servers_1479405e_d46c_47a2_91e8_eb43951c899f_networks_POST(self, method, url, body, headers):
         body = self.fixtures.load('network_to_node.json')
@@ -323,35 +325,49 @@ class GridscaleMockHttp(MockHttp):
         if method == 'GET':
             body = self.fixtures.load('ex_list_networks.json')
             return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
     def _objects_ips(self, method, url, body, headers):
         if method == 'GET':
             body = self.fixtures.load('ex_list_ips.json')
             return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
     def _objects_servers_1479405e_d46c_47a2_91e8_eb43951c899f_DELETE(self, method, url, body, headers):
         if method == 'DELETE':
             return (httplib.NO_CONTENT, None, {}, httplib.responses[httplib.NO_CONTENT])
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
     def _objects_storages_e66bb753_4a03_4ee2_a069_a601f393c9ee_DELETE(self, method, url, body, headers):
         if method == 'DELETE':
             return (httplib.NO_CONTENT, None, {}, httplib.responses[httplib.NO_CONTENT])
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
     def _objects_storages_e66bb753_4a03_4ee2_a069_a601f393c9ee_snapshots_d755de62_4d75_4d61_addd_a5c9743a5deb_DELETE(self, method, url, body, headers):
         if method == 'DELETE':
             return (httplib.NO_CONTENT, None, {}, httplib.responses[httplib.NO_CONTENT])
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
     def _objects_templates_POST(self, method, url, body, headers):
         # create_image
         if method == 'POST':
             body = self.fixtures.load('create_image.json')
             return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
     def _objects_storages_e66bb753_4a03_4ee2_a069_a601f393c9ee_snapshots_POST(self, method, url, body, headers):
         # create_image
         if method == 'POST':
             body = self.fixtures.load('create_image.json')
             return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
     def _objects_storages_e66bb753_4a03_4ee2_a069_a601f393c9ee_snapshots_690de890_13c0_4e76_8a01_e10ba8786e53_POST(self, method, url, body, headers):
         # create_image
@@ -363,12 +379,16 @@ class GridscaleMockHttp(MockHttp):
         if method == 'GET':
             body = self.fixtures.load('create_image_dict.json')
             return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
     def _objects_templates_12345(self, method, url, body, headers):
         # get_image
         if method == 'GET':
             body = self.fixtures.load('get_image.json')
             return (httplib.OK, body, {}, httplib.responses[httplib.OK])
+        else:  # pragma: no cover
+            raise ValueError('Invalid method')
 
 
 if __name__ == '__main__':
