9eb0bd040a3ff9ef0486ea0a300e65946951f7fb
Ryan Lee <ryanlee@zepheira.com>
2019-07-31 14:24:56 -0700

    reorder methods

--- libcloud/dns/drivers/gandi_live.py
+++ libcloud/dns/drivers/gandi_live.py
@@ -84,27 +84,6 @@ class GandiLiveDNSDriver(BaseGandiLiveDriver, DNSDriver):
         RecordType.WKS: 'WKS',
     }
 
-    def _to_zone(self, zone):
-        extra = {}
-        if 'zone_uuid' in zone:
-            extra = {
-                'zone_uuid': zone['zone_uuid']
-            }
-        return Zone(
-            id=str(zone['fqdn']),
-            domain=zone['fqdn'],
-            type='master',
-            ttl=0,
-            driver=self,
-            extra=extra,
-        )
-
-    def _to_zones(self, zones):
-        ret = []
-        for z in zones:
-            ret.append(self._to_zone(z))
-        return ret
-
     def list_zones(self):
         zones = self.connection.request(action='%s/domains' % API_BASE,
                                         method='GET')
@@ -149,105 +128,6 @@ class GandiLiveDNSDriver(BaseGandiLiveDriver, DNSDriver):
 
         return self._to_zone({'fqdn': domain, 'zone_uuid': new_zone_uuid})
 
-    # There is nothing you can update about a domain; you can update zones'
-    # names and which zone a domain is associated with, but the domain itself
-    # is basically immutable.  Instead, some ex_ methods for dealing with
-    # Gandi zones.
-
-    """
-    Update the name of a Gandi zone.
-
-    Note that a Gandi zone is not the same as a Libcloud zone.  A Gandi zone
-    is a separate object type from a Gandi domain; a Gandi zone can be reused
-    by multiple Gandi domains, and the actual records are associated with the
-    zone directly.  This is mostly masked in this driver to make it look like
-    records are associated with domains.  If you need to step out of that
-    masking, use these extension methods.
-
-    :param zone_uuid: Identifier for the Gandi zone.
-    :type  zone_uuid: ``str``
-
-    :param name: New name for the Gandi zone.
-    :type  name: ``str``
-
-    :return: ``bool``
-    """
-    # @@@ test
-    def ex_update_gandi_zone_name(self, zone_uuid, name):
-        action = '%s/zones/%s' % (API_BASE, zone_uuid)
-        data = {
-            'name': name,
-        }
-        self.connection.request(action=action, method='PATCH',
-                                data=data)
-        return True
-
-    # There is no concept of deleting domains in this API, not even to
-    # disassociate a domain from a zone.  You can delete a zone, though.
-    """
-    Delete a Gandi zone.  This may raise a ResourceConflictError if you
-    try to delete a zone that has domains still using it.
-
-    :param zone_uuid: Identifier for the Gandi zone
-    :type  zone_uuid: ``str``
-
-    :return: ``bool``
-    """
-    # @@@ test
-    def ex_delete_gandi_zone(self, zone_uuid):
-        self.connection.request(action='%s/zones/%s' % (API_BASE, zone_uuid),
-                                method='DELETE')
-        return True
-
-    """
-    Change the Gandi zone a domain is asociated with.
-
-    :param domain: Domain name to switch zones.
-    :type  domain: ``str``
-
-    :param zone_uuid: Identifier for the new Gandi zone to switch to.
-    :type  zone_uuid: ``str``
-
-    :return: ``bool``
-    """
-    def ex_switch_domain_gandi_zone(self, domain, zone_uuid):
-        domain_data = {
-            'zone_uuid': zone_uuid,
-        }
-        self.connection.request(action='%s/domains/%s' % (API_BASE, domain),
-                                method='PATCH',
-                                data=domain_data)
-        return True
-
-    # The Gandi service returns a set of values for a record,
-    # so setting something like extra['priority'] for MX records
-    # doesn't make a lot of sense.  Instead, the value is the
-    # first from the set, with extra['other_values'] for the
-    # remainder of values (not set if no other values).
-    def _to_record(self, record, zone):
-        extra = {
-            'ttl': int(record['rrset_ttl']),
-        }
-        rrset_values = record['rrset_values']
-        value = rrset_values[0]
-        if len(rrset_values) > 1:
-            extra['other_values'] = rrset_values[1:]
-        return Record(
-            id='%s:%s' % (record['rrset_type'], record['rrset_name']),
-            name=record['rrset_name'],
-            type=self._string_to_record_type(record['rrset_type']),
-            data=value,
-            zone=zone,
-            driver=self,
-            ttl=record['rrset_ttl'],
-            extra=extra)
-
-    def _to_records(self, records, zone):
-        retval = []
-        for r in records:
-            retval.append(self._to_record(r, zone))
-        return retval
-
     def list_records(self, zone):
         action = '%s/domains/%s/records' % (API_BASE, zone.id)
         records = self.connection.request(action=action, method='GET')
@@ -271,23 +151,6 @@ class GandiLiveDNSDriver(BaseGandiLiveDriver, DNSDriver):
                                           driver=self.connection.driver,
                                           record_id=record_id)
         return self._to_record(record.object, self.get_zone(zone_id))
-
-    def _validate_record(self, record_id, name, record_type, data, extra):
-        if len(data) > 1024:
-            raise RecordError('Record data must be <= 1024 characters',
-                              driver=self, record_id=record_id)
-        if extra is not None and 'other_values' in extra:
-            for other_value in other_values:
-                if len(other_value) > 1024:
-                    raise RecordError('Record data must be <= 1024 characters',
-                                      driver=self, record_id=record_id)
-        if extra is not None and 'ttl' in extra:
-            if extra['ttl'] < TTL_MIN:
-                raise RecordError('TTL must be at least 300 seconds',
-                                  driver=self, record_id=record_id)
-            if extra['ttl'] > TTL_MAX:
-                raise RecordError('TTL must not exceed 30 days',
-                                  driver=self, record_id=record_id)
 
     def create_record(self, name, zone, type, data, extra=None):
         self._validate_record(None, name, type, data, extra)
@@ -313,38 +176,6 @@ class GandiLiveDNSDriver(BaseGandiLiveDriver, DNSDriver):
                                                self.RECORD_TYPE_MAP[type],
                                                name))
 
-        return self._to_record(record_data, zone)
-
-    """
-    Create a new record with multiple values.
-
-    :param data: Record values (depends on the record type)
-    :type  data: ``list`` (of ``str``)
-    """
-    # @@@ test
-    def ex_create_multi_value_record(self, name, zone, type, data, extra=None):
-        self._validate_record(None, name, type, data, extra)
-
-        action = '%s/domains/%s/records' % (API_BASE, zone.id)
-
-        record_data = {
-            'rrset_name': name,
-            'rrset_type': self.RECORD_TYPE_MAP[type],
-            'rrset_values': data,
-        }
-
-        if 'ttl' in extra:
-            record_data['rrset_ttl'] = extra['ttl']
-
-        try:
-            self.connection.request(action=action, method='POST',
-                                    data=record_data)
-        except ResourceConflictError:
-            raise RecordAlreadyExistsError(value='',
-                                           driver=self.connection.driver,
-                                           record_id='%s:%s' % (
-                                               self.RECORD_TYPE_MAP[type],
-                                               name))
         return self._to_record(record_data, zone)
 
     """
@@ -425,3 +256,172 @@ class GandiLiveDNSDriver(BaseGandiLiveDriver, DNSDriver):
         resp = self.connection.request(action=action, method='GET',
                                        headers=headers, raw=True)
         return resp.body
+
+    # There is nothing you can update about a domain; you can update zones'
+    # names and which zone a domain is associated with, but the domain itself
+    # is basically immutable.  Instead, some ex_ methods for dealing with
+    # Gandi zones.
+
+    """
+    Update the name of a Gandi zone.
+
+    Note that a Gandi zone is not the same as a Libcloud zone.  A Gandi zone
+    is a separate object type from a Gandi domain; a Gandi zone can be reused
+    by multiple Gandi domains, and the actual records are associated with the
+    zone directly.  This is mostly masked in this driver to make it look like
+    records are associated with domains.  If you need to step out of that
+    masking, use these extension methods.
+
+    :param zone_uuid: Identifier for the Gandi zone.
+    :type  zone_uuid: ``str``
+
+    :param name: New name for the Gandi zone.
+    :type  name: ``str``
+
+    :return: ``bool``
+    """
+    # @@@ test
+    def ex_update_gandi_zone_name(self, zone_uuid, name):
+        action = '%s/zones/%s' % (API_BASE, zone_uuid)
+        data = {
+            'name': name,
+        }
+        self.connection.request(action=action, method='PATCH',
+                                data=data)
+        return True
+
+    # There is no concept of deleting domains in this API, not even to
+    # disassociate a domain from a zone.  You can delete a zone, though.
+    """
+    Delete a Gandi zone.  This may raise a ResourceConflictError if you
+    try to delete a zone that has domains still using it.
+
+    :param zone_uuid: Identifier for the Gandi zone
+    :type  zone_uuid: ``str``
+
+    :return: ``bool``
+    """
+    # @@@ test
+    def ex_delete_gandi_zone(self, zone_uuid):
+        self.connection.request(action='%s/zones/%s' % (API_BASE, zone_uuid),
+                                method='DELETE')
+        return True
+
+    """
+    Change the Gandi zone a domain is asociated with.
+
+    :param domain: Domain name to switch zones.
+    :type  domain: ``str``
+
+    :param zone_uuid: Identifier for the new Gandi zone to switch to.
+    :type  zone_uuid: ``str``
+
+    :return: ``bool``
+    """
+    def ex_switch_domain_gandi_zone(self, domain, zone_uuid):
+        domain_data = {
+            'zone_uuid': zone_uuid,
+        }
+        self.connection.request(action='%s/domains/%s' % (API_BASE, domain),
+                                method='PATCH',
+                                data=domain_data)
+        return True
+
+    """
+    Create a new record with multiple values.
+
+    :param data: Record values (depends on the record type)
+    :type  data: ``list`` (of ``str``)
+    """
+    # @@@ test
+    def ex_create_multi_value_record(self, name, zone, type, data, extra=None):
+        self._validate_record(None, name, type, data, extra)
+
+        action = '%s/domains/%s/records' % (API_BASE, zone.id)
+
+        record_data = {
+            'rrset_name': name,
+            'rrset_type': self.RECORD_TYPE_MAP[type],
+            'rrset_values': data,
+        }
+
+        if 'ttl' in extra:
+            record_data['rrset_ttl'] = extra['ttl']
+
+        try:
+            self.connection.request(action=action, method='POST',
+                                    data=record_data)
+        except ResourceConflictError:
+            raise RecordAlreadyExistsError(value='',
+                                           driver=self.connection.driver,
+                                           record_id='%s:%s' % (
+                                               self.RECORD_TYPE_MAP[type],
+                                               name))
+        return self._to_record(record_data, zone)
+
+    # The Gandi service returns a set of values for a record,
+    # so setting something like extra['priority'] for MX records
+    # doesn't make a lot of sense.  Instead, the value is the
+    # first from the set, with extra['other_values'] for the
+    # remainder of values (not set if no other values).
+    def _to_record(self, record, zone):
+        extra = {
+            'ttl': int(record['rrset_ttl']),
+        }
+        rrset_values = record['rrset_values']
+        value = rrset_values[0]
+        if len(rrset_values) > 1:
+            extra['other_values'] = rrset_values[1:]
+        return Record(
+            id='%s:%s' % (record['rrset_type'], record['rrset_name']),
+            name=record['rrset_name'],
+            type=self._string_to_record_type(record['rrset_type']),
+            data=value,
+            zone=zone,
+            driver=self,
+            ttl=record['rrset_ttl'],
+            extra=extra)
+
+    def _to_records(self, records, zone):
+        retval = []
+        for r in records:
+            retval.append(self._to_record(r, zone))
+        return retval
+
+    def _to_zone(self, zone):
+        extra = {}
+        if 'zone_uuid' in zone:
+            extra = {
+                'zone_uuid': zone['zone_uuid']
+            }
+        return Zone(
+            id=str(zone['fqdn']),
+            domain=zone['fqdn'],
+            type='master',
+            ttl=0,
+            driver=self,
+            extra=extra,
+        )
+
+    def _to_zones(self, zones):
+        ret = []
+        for z in zones:
+            ret.append(self._to_zone(z))
+        return ret
+
+    def _validate_record(self, record_id, name, record_type, data, extra):
+        if len(data) > 1024:
+            raise RecordError('Record data must be <= 1024 characters',
+                              driver=self, record_id=record_id)
+        if extra is not None and 'other_values' in extra:
+            for other_value in other_values:
+                if len(other_value) > 1024:
+                    raise RecordError('Record data must be <= 1024 characters',
+                                      driver=self, record_id=record_id)
+        if extra is not None and 'ttl' in extra:
+            if extra['ttl'] < TTL_MIN:
+                raise RecordError('TTL must be at least 300 seconds',
+                                  driver=self, record_id=record_id)
+            if extra['ttl'] > TTL_MAX:
+                raise RecordError('TTL must not exceed 30 days',
+                                  driver=self, record_id=record_id)
