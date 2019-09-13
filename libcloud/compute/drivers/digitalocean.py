43ef0a60898ae0fa932035f5be6abd22c0bc2724
mpempekos <spirostza77@hotmail.com>
2019-09-13 16:19:04 +0300

    Send NodeLocation objec in create_volume

--- libcloud/compute/drivers/digitalocean.py
+++ libcloud/compute/drivers/digitalocean.py
@@ -404,7 +404,7 @@ class DigitalOcean_v2_NodeDriver(DigitalOcean_v2_BaseDriver,
         :return: The newly created volume.
         :rtype: :class:`StorageVolume`
         """
-        attr = {'name': name, 'size_gigabytes': size, 'region': location,
+        attr = {'name': name, 'size_gigabytes': size, 'region': location.id,
                 'filesystem_type': filesystem_type}
 
         res = self.connection.request('/v2/volumes', data=json.dumps(attr),
