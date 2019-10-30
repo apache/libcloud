apache/libcloud

commit 6df2a52572b27d7efb1d8ae85220cc8ca46ccf88

Improve comment

libcloud/compute/drivers/openstack.py
@@ -2600,10 +2600,9 @@ class OpenStack_2_NodeDriver(OpenStack_1_1_NodeDriver):
     network_connectionCls = OpenStack_2_NetworkConnection
     network_connection = None
 
-    # Similarly not all node-related operations are exposed through the
-    # compute API
-    # See https://developer.openstack.org/api-ref/compute/
-    # For example, volume management are made in the cinder service
+    # Similarly all image operations are noe exposed through the block-storage
+    # API of the cinde service:
+    # https://developer.openstack.org/api-ref/block-storage/
     volumev2_connectionCls = OpenStack_2_VolumeV2Connection
     volumev2_connection = None
 

commit 9473a598c7f740001c98349baa11cedc0821c959

typo cinde -> cinder
introduced in https://github.com/apache/libcloud/commit/6df2a52572b27d7efb1d8ae85220cc8ca46ccf88

libcloud/compute/drivers/openstack.py
@@ -2684,7 +2684,7 @@ class OpenStack_2_NodeDriver(OpenStack_1_1_NodeDriver):
     network_connection = None
 
     # Similarly all image operations are noe exposed through the block-storage
-    # API of the cinde service:
+    # API of the cinder service:
     # https://developer.openstack.org/api-ref/block-storage/
     volumev2_connectionCls = OpenStack_2_VolumeV2Connection
     volumev2_connection = None