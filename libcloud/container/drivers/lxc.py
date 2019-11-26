f70cd3d5364d1976f3d1b957b8b2143747ed3dd8
David Hilbert <a.giavaras@gmail.com>
2019-11-26 16:28:54 +0200

    Add LXCContainerDriver class

---
+++ libcloud/container/drivers/lxc.py
@@ -0,0 +1,10 @@
+"""
+Module for handling LXC containers
+"""
+from libcloud.container.base import (Container, ContainerDriver, ContainerImage)
+
+class LXCContainerDriver(ContainerDriver):
+    """
+    Driver for LXC containers
+    """
+    pass
