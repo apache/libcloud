774586cea58e3fcaf66c8ab725bd97ee2a1e0733
Alex Giavaras <a.giavaras@gmail.com>
2019-12-11 18:25:03 +0200

    Bring tests closer to API responses

--- libcloud/test/container/test_lxd.py
+++ libcloud/test/container/test_lxd.py
@@ -72,6 +72,30 @@ class LXDContainerDriverTestCase(unittest.TestCase):
             container.start()
             self.assertEqual(container.state, 'running')
 
+    def test_stop_container(self):
+        for driver in self.drivers:
+            container = driver.get_container(id='second_lxd_container')
+            container.stop()
+            self.assertEqual(container.state, 'stopped')
+
+    def test_restart_container(self):
+        for driver in self.drivers:
+            container = driver.get_container(id='second_lxd_container')
+            container.restart()
+
+    def test_delete_container(self):
+        for driver in self.drivers:
+            container = driver.get_container(id='second_lxd_container')
+            container.destroy()
+
+    def test_deploy_container(self):
+        for driver in self.drivers:
+            container = driver.deploy_container(name='first_lxd_container',
+                                                image='54c8caac1f61901ed86c68f24af5f5d3672bdc62c71d04f06df3a59e95684473',
+                                                parameters={})
+            self.assertIsInstance(container, Container)
+            self.assertEqual(container.name, 'first_lxd_container')
+
 
 class LXDMockHttp(MockHttp):
     fixtures = ContainerFileFixtures('lxd')
@@ -104,7 +128,13 @@ class LXDMockHttp(MockHttp):
 
     def _linux_124_containers(
         self, method, url, body, headers):
-        return (httplib.OK, self.fixtures.load('linux_124/containers.json'), {}, httplib.responses[httplib.OK])
+
+        if method == 'GET':
+            return (httplib.OK, self.fixtures.load('linux_124/containers.json'), {}, httplib.responses[httplib.OK])
+        elif method == 'POST' or method == 'PUT':
+            # we do a POST when we create a container
+            # we will return a dummy background operation
+            return (httplib.OK, self.fixtures.load('linux_124/background_op.json'), {}, httplib.responses[httplib.OK])
 
     def _linux_124_containers_first_lxd_container(
         self, method, url, body, headers):
@@ -115,75 +145,50 @@ class LXDMockHttp(MockHttp):
         return (httplib.OK, self.fixtures.load('linux_124/second_lxd_container.json'), {}, httplib.responses[httplib.OK])
 
     def _linux_124_containers_first_lxd_container_state(self, method, url, body, headers):
-        json = self.fixtures.load('linux_124/first_lxd_container.json')
-        return (httplib.OK, json, {}, httplib.responses[httplib.OK])
+
+        if method == 'PUT':
+            json = self.fixtures.load('linux_124/background_op.json')
+            return (httplib.OK, json, {}, httplib.responses[httplib.OK])
+        elif method == 'GET':
+            json = self.fixtures.load('linux_124/first_lxd_container.json')
+            return (httplib.OK, json, {}, httplib.responses[httplib.OK])
+
+    def _linux_124_containers_second_lxd_container_state(self, method, url, body, headers):
+
+        if method == 'PUT':
+            json = self.fixtures.load('linux_124/background_op.json')
+            return (httplib.OK, json, {}, httplib.responses[httplib.OK])
+        elif method == 'GET':
+            json = self.fixtures.load('linux_124/second_lxd_container.json')
+            return (httplib.OK, json, {}, httplib.responses[httplib.OK])
 
     def _vlinux_124_containers_create(
         self, method, url, body, headers):
         return (httplib.OK, self.fixtures.load('linux_124/create_container.json'), {}, httplib.responses[httplib.OK])
-    """
-    def _vmac_124_containers_create(
-        self, method, url, body, headers):
-        return (httplib.OK, self.fixtures.load('mac_124/create_container.json'), {}, httplib.responses[httplib.OK])
-    """
 
     def _vlinux_124_containers_a68c1872c74630522c7aa74b85558b06824c5e672cee334296c50fb209825303(
         self, method, url, body, headers):
         return (httplib.NO_CONTENT, '', {}, httplib.responses[httplib.OK])
 
-    """
-    def _vmac_124_containers_a68c1872c74630522c7aa74b85558b06824c5e672cee334296c50fb209825303(
-        self, method, url, body, headers):
-        return (httplib.NO_CONTENT, '', {}, httplib.responses[httplib.OK])
-    """
     def _vlinux_124_containers_a68c1872c74630522c7aa74b85558b06824c5e672cee334296c50fb209825303_start(
         self, method, url, body, headers):
         return (httplib.NO_CONTENT, '', {}, httplib.responses[httplib.OK])
 
-    """
-    def _vmac_124_containers_a68c1872c74630522c7aa74b85558b06824c5e672cee334296c50fb209825303_start(
-        self, method, url, body, headers):
-        return (httplib.NO_CONTENT, '', {}, httplib.responses[httplib.OK])
-    """
-
     def _vlinux_124_containers_a68c1872c74630522c7aa74b85558b06824c5e672cee334296c50fb209825303_restart(
         self, method, url, body, headers):
         return (httplib.NO_CONTENT, '', {}, httplib.responses[httplib.OK])
-    """
-    def _vmac_124_containers_a68c1872c74630522c7aa74b85558b06824c5e672cee334296c50fb209825303_restart(
-        self, method, url, body, headers):
-        return (httplib.NO_CONTENT, '', {}, httplib.responses[httplib.OK])
-    """
 
     def _vlinux_124_containers_a68c1872c74630522c7aa74b85558b06824c5e672cee334296c50fb209825303_rename(
         self, method, url, body, headers):
         return (httplib.NO_CONTENT, '', {}, httplib.responses[httplib.OK])
 
-    """
-    def _vmac_124_containers_a68c1872c74630522c7aa74b85558b06824c5e672cee334296c50fb209825303_rename(
-        self, method, url, body, headers):
-        return (httplib.NO_CONTENT, '', {}, httplib.responses[httplib.OK])
-    """
-
     def _vlinux_124_containers_a68c1872c74630522c7aa74b85558b06824c5e672cee334296c50fb209825303_stop(
         self, method, url, body, headers):
         return (httplib.NO_CONTENT, '', {}, httplib.responses[httplib.OK])
 
-    """
-    def _vmac_124_containers_a68c1872c74630522c7aa74b85558b06824c5e672cee334296c50fb209825303_stop(
-        self, method, url, body, headers):
-        return (httplib.NO_CONTENT, '', {}, httplib.responses[httplib.OK])
-    """
-
     def _vlinux_124_containers_a68c1872c74630522c7aa74b85558b06824c5e672cee334296c50fb209825303_json(
         self, method, url, body, headers):
         return (httplib.OK, self.fixtures.load('linux_124/container_a68.json'), {}, httplib.responses[httplib.OK])
-
-    """
-    def _vmac_124_containers_a68c1872c74630522c7aa74b85558b06824c5e672cee334296c50fb209825303_json(
-        self, method, url, body, headers):
-        return (httplib.OK, self.fixtures.load('linux_124/container_a68.json'), {}, httplib.responses[httplib.OK])
-    """
 
     def _vlinux_124_containers_a68c1872c74630522c7aa74b85558b06824c5e672cee334296c50fb209825303_logs(
         self, method, url, body, headers):
