f3596eca8331191abd5a1aad5c3a4e88bccb29d5
Clemens Wolff <clewolff@microsoft.com>
2019-12-29 13:48:47 -0500

    Make test work on Windows

--- libcloud/test/storage/test_local.py
+++ libcloud/test/storage/test_local.py
@@ -110,7 +110,9 @@ class LocalTests(unittest.TestCase):
         objects = self.driver.list_container_objects(container=container)
         self.assertEqual(len(objects), 5)
 
-        objects = self.driver.list_container_objects(container=container, prefix='path/to/')
+        prefix = os.path.join('path', 'to')
+        objects = self.driver.list_container_objects(container=container,
+                                                     prefix=prefix)
         self.assertEqual(len(objects), 2)
 
         for obj in objects:
