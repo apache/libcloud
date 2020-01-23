2ce468c7b838e9df68819bbb7f1177eb68382280
Ryan Lee <ryanlee@zepheira.com>
2020-01-22 16:41:26 -0800

    Update Gandi Live DNS driver to match service change in HTTP DELETE response

--- libcloud/common/gandi_live.py
+++ libcloud/common/gandi_live.py
@@ -139,13 +139,18 @@ class GandiLiveResponse(JsonResponse):
         valid_http_codes = [
             httplib.OK,
             httplib.CREATED,
-            httplib.NO_CONTENT
         ]
         if self.status in valid_http_codes:
             if json_error:
                 raise JsonParseError(body, self.status)
             else:
                 return body
+        elif self.status == httplib.NO_CONTENT:
+            # Parse error for empty body is acceptable, but a non-empty body is not.
+            if len(body) > 0:
+                raise GandiLiveBaseError('"No Content" response contained content', self.status)
+            else:
+                return {}
         elif self.status == httplib.NOT_FOUND:
             message = self._get_error(body, json_error)
             raise ResourceNotFoundError(message, self.status)
