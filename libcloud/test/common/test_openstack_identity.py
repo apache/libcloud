1daec1154492399c71c9f672a18b6c9d5d858d52
Tomaz Muraus <tomaz@tomaz.me>
2020-03-10 23:00:22 +0100

    Add a test case for invalid project id being specified.

--- libcloud/test/common/test_openstack_identity.py
+++ libcloud/test/common/test_openstack_identity.py
@@ -536,7 +536,7 @@ class OpenStackIdentity_3_0_Connection_OIDC_access_token_project_idTests(
                                                                                 domain_name='project_id2')
         self.auth_instance.auth_token = 'mock'
 
-    def test_authenticate(self):
+    def test_authenticate_valid_project_id(self):
         auth = OpenStackIdentity_3_0_Connection_OIDC_access_token(auth_url='http://none',
                                                                   user_id='idp',
                                                                   key='token',
@@ -544,6 +544,17 @@ class OpenStackIdentity_3_0_Connection_OIDC_access_token_project_idTests(
                                                                   tenant_name="oidc",
                                                                   domain_name='project_id2')
         auth.authenticate()
+
+    def test_authenticate_invalid_project_id(self):
+        auth = OpenStackIdentity_3_0_Connection_OIDC_access_token(auth_url='http://none',
+                                                                  user_id='idp',
+                                                                  key='token',
+                                                                  token_scope='project',
+                                                                  tenant_name="oidc",
+                                                                  domain_name='project_id100')
+
+        expected_msg = 'Project project_id100 not found'
+        self.assertRaisesRegex(ValueError, expected_msg, auth.authenticate)
 
 
 class OpenStackIdentity_2_0_Connection_VOMSTests(unittest.TestCase):
