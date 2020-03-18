6de9db48a4500332531d7a7c0727c7cf92002a30
Tomaz Muraus <tomaz@tomaz.me>
2020-03-18 22:06:18 +0100

    Update GCE docs and make it clear that a lot of operations rely on zone / location being set.
    Also update examples to always include datancenter constructor argument.

--- docs/examples/compute/gce/gce_installed_application.py
+++ docs/examples/compute/gce/gce_installed_application.py
@@ -3,4 +3,5 @@ from libcloud.compute.providers import get_driver
 
 ComputeEngine = get_driver(Provider.GCE)
 driver = ComputeEngine('your_client_id', 'your_client_secret',
+                       datacenter='us-central1-a',
                        project='your_project_id')
