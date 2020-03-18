a84ca060e4c9d4f40403d6dfbffc731a8e6b5b4f
Ori Hoch <ori@uumpa.com>
2020-03-18 15:25:30 +0200

    finalize kamatera tests

--- libcloud/test/compute/test_kamatera.py
+++ libcloud/test/compute/test_kamatera.py
@@ -44,8 +44,8 @@ class KamateraAuthenticationTests(LibcloudTestCase):
 class KamateraNodeDriverTests(LibcloudTestCase):
 
     def setUp(self):
-        # KamateraNodeDriver.connectionCls.conn_class = KamateraMockHttp
-        self.driver = KamateraNodeDriver(*KAMATERA_PARAMS)
+        KamateraTestDriver.connectionCls.conn_class = KamateraMockHttp
+        self.driver = KamateraTestDriver(*KAMATERA_PARAMS)
         self.eu_node_location = NodeLocation(
             id='EU',
             name='Amsterdam',
@@ -65,16 +65,13 @@ class KamateraNodeDriverTests(LibcloudTestCase):
                    'code': '8.0 64bit_minimal',
                    'osDiskSizeGB': 5,
                    "ramMBMin": {"A": 256, "B": 256, "T": 256, "D": 256}})
-        self.small_node_size = NodeSize(
-            id='small',
-            name='small',
-            ram=4096,
-            disk=30,
-            bandwidth=0,
-            price=0,
-            driver=self.driver,
-            extra={'cpuType': 'B', 'cpuCores': 2, 'monthlyTrafficPackage': 't5000',
-                   'extraDiskSizesGB': []})
+        self.small_node_size = self.driver.ex_get_size(ramMB=4096,
+                                                       diskSizeGB=30,
+                                                       cpuType='B',
+                                                       cpuCores=2,
+                                                       monthlyTrafficPackage='t5000',
+                                                       id='small',
+                                                       name='small')
 
     def test_creating_driver(self):
         cls = providers.get_driver(Provider.KAMATERA)
@@ -83,6 +80,8 @@ class KamateraNodeDriverTests(LibcloudTestCase):
     def test_features(self):
         features = self.driver.features['create_node']
         self.assertIn('password', features)
+        self.assertIn('generates_password', features)
+        self.assertIn('ssh_key', features)
 
     def test_list_locations(self):
         locations = self.driver.list_locations()
@@ -109,42 +108,42 @@ class KamateraNodeDriverTests(LibcloudTestCase):
 
     def test_create_node(self):
         node = self.driver.create_node(name='test_server', size=self.small_node_size,
-                                       image=self.centos_8_EU_node_image, location=self.eu_node_location,
-                                       )
+                                       image=self.centos_8_EU_node_image, location=self.eu_node_location)
 
-        self.assertTrue(re.match('^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$', node.id))
-        self.assertEqual(node.name, 'test_server')
-        self.assertEqual(node.state, NodeState.STARTING)
+        self.assertTrue(len(node.id) > 8)
+        self.assertEqual(node.name, 'my-server')
+        self.assertEqual(node.state, NodeState.RUNNING)
         self.assertTrue(len(node.public_ips) > 0)
         self.assertTrue(len(node.private_ips) > 0)
         self.assertEqual(node.driver, self.driver)
-        self.assertTrue(len(node.extra['password']) > 0)
-        self.assertTrue(len(node.extra['vnc_password']) > 0)
+        self.assertTrue(len(node.extra['generated_password']) > 0)
 
     def test_create_node_with_ssh_keys(self):
-        image = NodeImage(id='01000000-0000-4000-8000-000030060200',
-                          name='Ubuntu Server 16.04 LTS (Xenial Xerus)',
-                          extra={'type': 'template'},
-                          driver=self.driver)
-        location = NodeLocation(id='fi-hel1', name='Helsinki #1', country='FI', driver=self.driver)
-        size = NodeSize(id='1xCPU-1GB', name='1xCPU-1GB', ram=1024, disk=30, bandwidth=2048,
-                        extra={'storage_tier': 'maxiops'}, price=None, driver=self.driver)
+        node = self.driver.create_node(name='test_server_pubkey', size=self.small_node_size,
+                                       image=self.centos_8_EU_node_image, location=self.eu_node_location,
+                                       auth=NodeAuthSSHKey('publickey'))
 
-        auth = NodeAuthSSHKey('publikey')
-        node = self.driver.create_node(name='test_server', size=size, image=image, location=location, auth=auth)
-        self.assertTrue(re.match('^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$', node.id))
-        self.assertEqual(node.name, 'test_server')
-        self.assertEqual(node.state, NodeState.STARTING)
+        self.assertTrue(len(node.id) > 8)
+        self.assertEqual(node.name, 'my-server')
+        self.assertEqual(node.state, NodeState.RUNNING)
         self.assertTrue(len(node.public_ips) > 0)
         self.assertTrue(len(node.private_ips) > 0)
         self.assertEqual(node.driver, self.driver)
+        self.assertFalse('generated_password' in node.extra)
 
     def test_list_nodes(self):
         nodes = self.driver.list_nodes()
-
         self.assertTrue(len(nodes) >= 1)
         node = nodes[0]
         self.assertEqual(node.name, 'test_server')
+        self.assertEqual(node.state, NodeState.RUNNING)
+        self.assertEqual(node.driver, self.driver)
+
+    def test_list_nodes_full(self):
+        nodes = self.driver.list_nodes(ex_full_details=True)
+        self.assertTrue(len(nodes) >= 1)
+        node = nodes[0]
+        self.assertEqual(node.name, 'my-server')
         self.assertEqual(node.state, NodeState.RUNNING)
         self.assertTrue(len(node.public_ips) > 0)
         self.assertTrue(len(node.private_ips) > 0)
@@ -153,15 +152,6 @@ class KamateraNodeDriverTests(LibcloudTestCase):
     def test_reboot_node(self):
         nodes = self.driver.list_nodes()
         success = self.driver.reboot_node(nodes[0])
-        self.assertTrue(success)
-
-    def test_destroy_node(self):
-        if KamateraNodeDriver.connectionCls.conn_class == KamateraMockHttp:
-            nodes = [Node(id='00893c98_5d5a_4363_b177_88df518a2b60', name='', state='',
-                          public_ips=[], private_ips=[], driver=self.driver)]
-        else:
-            nodes = self.driver.list_nodes()
-        success = self.driver.destroy_node(nodes[0])
         self.assertTrue(success)
 
     def assert_object(self, expected_object, objects):
@@ -189,14 +179,17 @@ class KamateraNodeDriverTests(LibcloudTestCase):
             return False
 
         for key in d1.keys():
-            if isinstance(d1[key], KamateraMonthlyTrafficPackage):
-                if d1[key].id != d2[key].id or d1[key].description != d2[key].description:
-                    return False
-            elif d1[key] != d2[key]:
+            if d1[key] != d2[key]:
                 return False
 
         return True
 
+
+class KamateraTestDriver(KamateraNodeDriver):
+
+    def ex_wait_command(self, *args, **kwargs):
+        kwargs['poll_interval_seconds'] = 0
+        return KamateraNodeDriver.ex_wait_command(self, *args, **kwargs)
 
 class KamateraMockHttp(MockHttp):
 
@@ -208,15 +201,48 @@ class KamateraMockHttp(MockHttp):
             body = self.fixtures.load('failed_auth.json')
             status = httplib.UNAUTHORIZED
         else:
-            body = self.fixtures.load({
-                '/service/server?datacenter=1': 'datacenters.json',
-                '/service/server?sizes=1&datecenter=EU': 'sizes_datacenter_EU.json',
-                '/service/server?images=1&datacenter=EU': 'images_datacenter_EU.json',
-                '/service/server?images=1&datacenter=': 'images_datacenter_EU.json',
-                '/service/server?capabilities=1&datacenter=EU': 'capabilities_datacenter_EU.json',
-                '/service/server': 'create_Server.json'
-            }[url])
+            if url == '/service/server' and json.loads(body).get('ssh-key'):
+                body = self.fixtures.load('create_server_sshkey.json')
+            else:
+                body = self.fixtures.load({
+                    '/service/server?datacenter=1': 'datacenters.json',
+                    '/service/server?sizes=1&datacenter=EU': 'sizes_datacenter_EU.json',
+                    '/service/server?images=1&datacenter=EU': 'images_datacenter_EU.json',
+                    '/service/server?capabilities=1&datacenter=EU': 'capabilities_datacenter_EU.json',
+                    '/service/server': 'create_server.json'
+                }[url])
             status = httplib.OK
+        return status, body, {}, httplib.responses[status]
+
+    def _service_queue(self, method, url, body, headers):
+        if not hasattr(self, '_service_queue_call_count'):
+            self._service_queue_call_count = 0
+        self._service_queue_call_count += 1
+        body = self.fixtures.load({
+            '/service/queue?id=12345': 'queue_12345-%s.json' % self._service_queue_call_count
+        }[url])
+        status = httplib.OK
+        return status, body, {}, httplib.responses[status]
+
+    def _service_server_info(self, method, url, body, headers):
+        body = self.fixtures.load({
+            "/service/server/info": "server_info.json"
+        }[url])
+        status = httplib.OK
+        return status, body, {}, httplib.responses[status]
+
+    def _service_servers(self, method, url, body, headers):
+        body = self.fixtures.load({
+            '/service/servers': 'servers.json'
+        }[url])
+        status = httplib.OK
+        return status, body, {}, httplib.responses[status]
+
+    def _service_server_reboot(self, method, url, body, headers):
+        body = self.fixtures.load({
+            '/service/server/reboot': 'server_operation.json'
+        }[url])
+        status = httplib.OK
         return status, body, {}, httplib.responses[status]
 
 
