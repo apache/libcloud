9d3ad46951af5252f74b31cca26b9e3c888034a3
Tomaz Muraus <tomaz@tomaz.me>
2020-06-01 17:00:50 +0200

    Add a test case for Gandi Live DNS driver.

--- libcloud/test/dns/test_gandi_live.py
+++ libcloud/test/dns/test_gandi_live.py
@@ -119,6 +119,14 @@ class GandiLiveTests(unittest.TestCase):
         self.assertEqual(record.type, RecordType.AAAA)
         self.assertEqual(record.data, '::1')
 
+    def test_create_record_doesnt_throw_if_ttl_is_not_provided(self):
+        record = self.driver.create_record('alice', self.test_zone, 'AAAA',
+                                           '::1')
+        self.assertEqual(record.id, 'AAAA:alice')
+        self.assertEqual(record.name, 'alice')
+        self.assertEqual(record.type, RecordType.AAAA)
+        self.assertEqual(record.data, '::1')
+
     def test_bad_record_validation(self):
         with self.assertRaises(RecordError) as ctx:
             self.driver.create_record('alice', self.test_zone, 'AAAA',
