350c6d6462c4052a0433bc4b6fd46b47bd01c8ef
Tomaz Muraus <tomaz@tomaz.me>
2020-06-02 14:06:49 +0200

    Fix lint.

--- docs/examples/dns/create_record_caa_record_type.py
+++ docs/examples/dns/create_record_caa_record_type.py
@@ -9,10 +9,13 @@ driver = cls(*CREDENTIALS)
 zone = [z for z in driver.list_zones() if z.domain == 'example.com'][0]
 
 # 1. issue tag
-print(zone.create_record(name='www', type=RecordType.CAA, data='0 issue caa.domain.com'))
+print(zone.create_record(name='www', type=RecordType.CAA,
+                         data='0 issue caa.domain.com'))
 
 # 2. issuewild tag
-record = zone.create_record(name='www', type=RecordType.CAA, data='0 issuewild caa.domain.com')
+record = zone.create_record(name='www', type=RecordType.CAA
+                            data='0 issuewild caa.domain.com')
 
 # 3. iodef tag
-record = zone.create_record(name='www', type=RecordType.CAA, data='0 iodef caa.domain.com/report')
+record = zone.create_record(name='www', type=RecordType.CAA,
+                            data='0 iodef caa.domain.com/report')
