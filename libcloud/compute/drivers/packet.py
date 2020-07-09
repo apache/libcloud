7dbdc7881ef891d5ab439c193f1b205bdaf3e236
mpempekos <spirostza77@hotmail.com>
2020-07-09 18:49:38 +0300

    Fix extracting regions in size.extra. Use replace instead of strip

--- libcloud/compute/drivers/packet.py
+++ libcloud/compute/drivers/packet.py
@@ -428,7 +428,7 @@ def _list_async(driver):
             cpus = data['specs']['cpus'][0].get('count')
         except KeyError:
             cpus = None
-        regions = [region.get('href').strip('/facilities/')
+        regions = [region.get('href').replace('/facilities/', '')
             for region in data.get('available_in')]
         extra = {'description': data['description'], 'line': data['line'],
                  'cpus': cpus, 'regions': regions}
