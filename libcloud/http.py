4b70b76f6946e6ea4892c1df532b0cf5d51ca36e
Michael Spagon <mspagon@infinite.io>
2020-08-27 14:43:00 -0500

    Fixes an issue where HTTP requests with an empty body are being chunked and causing HTTP 400 errors.

--- libcloud/http.py
+++ libcloud/http.py
@@ -241,8 +241,6 @@ class LibcloudConnection(LibcloudBaseConnection):
 
         prepped = self.session.prepare_request(req)
 
-        prepped.body = body
-
         self.response = self.session.send(
             prepped,
             stream=stream,
