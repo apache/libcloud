6c387e4100cb3e50c1cf62ba675ce176d636b14a
Pierre AILLET <pierre.aillet@outscale.com>
2020-09-21 17:43:51 +0200

    Add new accounts features for outscale provider

--- libcloud/compute/drivers/outscale.py
+++ libcloud/compute/drivers/outscale.py
@@ -1056,6 +1056,46 @@ class OutscaleNodeDriver(NodeDriver):
         data = json.dumps({"DryRun": dry_run})
         return self._call_api(action, data).json()["Accounts"][0]
 
+    def ex_list_consumption_account(
+        self,
+        from_date: str = None,
+        to_date: str = None,
+        dry_run: bool = False
+        ):
+        """
+        Displays information about the consumption of your account for 
+        each billable resource within the specified time period.
+
+
+        :param      from_date: The beginning of the time period, in 
+        ISO 8601 date-time format (for example, 2017-06-14 or 
+        2017-06-14T00:00:00Z). (required)
+        :type       from_date: ``str``
+
+        :param      to_date: The end of the time period, in 
+        ISO 8601 date-time format (for example, 2017-06-30 or 
+        2017-06-30T00:00:00Z). (required)
+        :type       to_date: ``str``
+
+        :param      dry_run: If true, checks whether you have the required
+        permissions to perform the action.
+        :type       dry_run: ``bool``
+
+        :return: a list of Consumption Entries
+        :rtype: ``list`` of ``dict``
+        """
+        action = "ReadConsumptionAccount"
+        data = {"DryRun": dry_run}
+        if from_date is not None:
+            data.update({"FromDate": from_date})
+        if to_date is not None:
+            data.update({"ToDate": to_date})
+        print(data)
+        response = self._call_api(action, json.dumps(data))
+        if response.status_code == 200:
+            return response.json()
+        return response.status_code
+
     def ex_create_account(
         self,
         city: str = None,
