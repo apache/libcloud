8509bdd29aad1039ccc1640fe7cb98927d6bb13d
Tio Gobin <tio.gobin@outscale.com>
2020-10-05 16:26:58 +0200

    docstring update for outscale provider

--- libcloud/compute/drivers/outscale.py
+++ libcloud/compute/drivers/outscale.py
@@ -6830,22 +6830,6 @@ class OutscaleNodeDriver(NodeDriver):
 
         :param      rules: Information about the security group rule to create:
         https://docs.outscale.com/api#createsecuritygrouprule
-        rules = [
-           {
-                "FromPortRange": int,
-                "IpProtocol": str,
-                "IpRanges": [str],
-                "SecurityGroupsMembers": [
-                    {
-                        "AccountId": str,
-                        "SecurityGroupIds": str,
-                        "SecurityGroupName": str
-                    }
-                ],
-                "ServiceIds": [str],
-                "ToPortRange": int,
-           }
-        ]
         :type       rules: ``list`` of  ``dict``
 
         :param      sg_account_id_to_unlink: The account ID of the
