ec9bd4f02bee197cec1208af0e9e6d4223ff8f27
Tomaz Muraus <tomaz@tomaz.me>
2020-11-01 21:49:28 +0100

    Clarify that no de-normalization is needed for SSHFP records on response.

--- libcloud/dns/drivers/cloudflare.py
+++ libcloud/dns/drivers/cloudflare.py
@@ -451,6 +451,12 @@ class CloudFlareDNSDriver(DNSDriver):
         """
         Normalize record data for "special" records such as CAA and SSHFP
         so it can be used with the CloudFlare API.
+
+        Keep ind mind that value for SSHFP record type onluy needs to be
+        normalized for the create / update operations.
+
+        On list operation (aka response), actual value is returned
+        normally in the "content" attribute.
         """
         cf_data = {}
         if not data:
