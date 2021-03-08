65487e6362a415a2d069a1d799eb857622fb313e
Miguel Caballer <micafer1@upv.es>
2021-03-08 14:03:11 +0100

    Change test

--- libcloud/test/compute/test_gce.py
+++ libcloud/test/compute/test_gce.py
@@ -729,14 +729,14 @@ class GCENodeDriverTest(GoogleTestCase, TestCaseMixin):
     def test_list_sizes(self):
         sizes = self.driver.list_sizes()
         sizes_all = self.driver.list_sizes('all')
-        self.assertEqual(len(sizes), 22)
-        self.assertEqual(len(sizes_all), 101)
+        self.assertEqual(len(sizes), 23)
+        self.assertEqual(len(sizes_all), 100)
         self.assertEqual(sizes[0].name, 'f1-micro')
         self.assertEqual(sizes[0].extra['zone'].name, 'us-central1-a')
-        self.assertEqual(sizes_all[20].name, 'a2-highgpu-1g')
-        self.assertEqual(len(sizes_all[20].extra['accelerators']), 1)
+        self.assertEqual(sizes[1].name, 'a2-highgpu-1g')
+        self.assertEqual(len(sizes[1].extra['accelerators']), 1)
         self.assertEqual(
-            sizes_all[20].extra['accelerators'][0]['guestAcceleratorType'],
+            sizes[1].extra['accelerators'][0]['guestAcceleratorType'],
             "nvidia-tesla-v100")
         names = [s.name for s in sizes_all]
         self.assertEqual(names.count('n1-standard-1'), 5)
