45da05195f0defb524499932e60fe60978654a4a
Christos Karageorgiou Kaneen <csk@mist.io>
2021-06-15 15:11:27 +0000

    Add `resources` to Container `extra` attribute

--- libcloud/container/drivers/kubernetes.py
+++ libcloud/container/drivers/kubernetes.py
@@ -362,6 +362,13 @@ class KubernetesContainerDriver(KubernetesDriverMixin, ContainerDriver):
             if started_at:
                 created_at = datetime.datetime.strptime(
                     started_at, '%Y-%m-%dT%H:%M:%SZ')
+        extra = {
+            'pod': pod_data['metadata']['name'],
+            'namespace': pod_data['metadata']['namespace']
+        }
+        resources = data.get('resources')
+        if resources:
+            extra['resources'] = resources
         return Container(
             id=container_status.get('containerID') or data['name'],
             name=data['name'],
@@ -376,10 +383,7 @@ class KubernetesContainerDriver(KubernetesDriverMixin, ContainerDriver):
                    if container_status else ContainerState.UNKNOWN),
             driver=self.connection.driver,
             created_at=created_at,
-            extra={
-                'pod': pod_data['metadata']['name'],
-                'namespace': pod_data['metadata']['namespace']
-            })
+            extra=extra)
 
     def _to_cluster(self, data):
         """
