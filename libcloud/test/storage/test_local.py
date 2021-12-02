c2b870b6c3649ad5aab50a468411025975f56039
Tomaz Muraus <tomaz@tomaz.me>
2021-12-02 20:01:59 +0100

    Fix tox target, make sure locking is enabled for the tests.

--- libcloud/test/storage/test_local.py
+++ libcloud/test/storage/test_local.py
@@ -34,6 +34,9 @@ from libcloud.storage.types import InvalidContainerNameError
 from libcloud.utils.files import exhaust_iterator
 
 try:
+    import libcloud.storage.drivers.local
+    libcloud.storage.drivers.local.USE_LOCKING = True
+
     from libcloud.storage.drivers.local import LocalStorageDriver
     from libcloud.storage.drivers.local import LockLocalStorage
     import fasteners  # noqa
