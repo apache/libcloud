a961757a9c62db4727e0aa05174e0336fc81e705
Rainer 'rei' Schuth <rei@reixd.net>
2021-12-05 02:25:21 +0100

    Add documentation and example based on the Minio docs

---
+++ docs/examples/storage/scaleway/upload_example.py
@@ -0,0 +1,16 @@
+from libcloud.storage.types import Provider
+from libcloud.storage.providers import get_driver
+
+FILE_PATH = "/home/user/myfile.tar.gz"
+
+cls = get_driver(Provider.SCALEWAY)
+driver = cls("api key", "api secret key", host="s3.fr-par.scw.cloud")
+
+container = driver.get_container(container_name="<your-bucket-name>")
+
+extra = {"meta_data": {"owner": "myuser", "created": "2001-05-25"}}
+
+with open(FILE_PATH, "rb") as iterator:
+    obj = driver.upload_object_via_stream(
+        iterator=iterator, container=container, object_name="backup.tar.gz", extra=extra
+    )
