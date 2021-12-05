a158138990d6e1dd5f2f4c970656fd5af2d7ff90
Rainer 'rei' Schuth <rei@reixd.net>
2021-12-05 23:18:12 +0100

    Improve test case

--- docs/examples/storage/scaleway/upload_example.py
+++ docs/examples/storage/scaleway/upload_example.py
@@ -4,11 +4,15 @@ from libcloud.storage.providers import get_driver
 FILE_PATH = "/home/user/myfile.tar.gz"
 
 cls = get_driver(Provider.SCALEWAY)
-driver = cls("api key", "api secret key", host="s3.fr-par.scw.cloud")
+
+driver = cls("api key", "api secret key", region="fr-par")
 
 container = driver.get_container(container_name="<your-bucket-name>")
 
-extra = {"meta_data": {"owner": "myuser", "created": "2001-05-25"}}
+extra = {
+    "meta_data": {"owner": "myuser", "created": "2001-05-25"},
+    "acl": "public-read"
+}
 
 with open(FILE_PATH, "rb") as iterator:
     obj = driver.upload_object_via_stream(
