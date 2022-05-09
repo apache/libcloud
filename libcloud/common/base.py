a6ec3212af3d9115ef655a050a5990e191fa6580
Logovskii Dmitrii <d-zaytsev@yandex-team.ru>
2022-05-09 15:24:22 +0300

    make new connection importable

--- libcloud/common/base.py
+++ libcloud/common/base.py
@@ -46,6 +46,7 @@ __all__ = [
     "PollingConnection",
     "ConnectionKey",
     "ConnectionUserAndKey",
+    "ConnectionUserPasswordAndKey",
     "CertificateConnection",
     "Response",
     "HTTPResponse",
