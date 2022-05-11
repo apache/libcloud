95753dadf64128f10b75c07b9944d04101ad89b8
Olivier Gayot <olivier.gayot@canonical.com>
2022-05-11 15:25:58 +0200

    test/test_connection.py: allow tests to run if https_proxy is set
    We have code in setUp & tearDown functions to make tests run properly if
    the http_proxy variable is already present in the environment when
    running tests.
    
    The code backs up the http_proxy variable, clears it, and restores it
    after the tests have run.
    
    However, we would also need to handle the HTTPS counterpart, the
    https_proxy variable. Running tests with this variable set currently
    fails with:
    
          self.assertIsNone(conn.proxy_scheme)
    E       AssertionError: 'http' is not None
    
    libcloud/test/test_connection.py:127: AssertionError
    
    This patch adds the same logic that we have for HTTP, but for the HTTPS
    counterpart.
    
    Signed-off-by: Olivier Gayot <olivier.gayot@canonical.com>

--- libcloud/test/test_connection.py
+++ libcloud/test/test_connection.py
@@ -40,13 +40,19 @@ from libcloud.utils.retry import RetryForeverOnRateLimitError
 
 class BaseConnectionClassTestCase(unittest.TestCase):
     def setUp(self):
-        self.orig_proxy = os.environ.pop("http_proxy", None)
+        self.orig_http_proxy = os.environ.pop("http_proxy", None)
+        self.orig_https_proxy = os.environ.pop("https_proxy", None)
 
     def tearDown(self):
-        if self.orig_proxy:
-            os.environ["http_proxy"] = self.orig_proxy
+        if self.orig_http_proxy:
+            os.environ["http_proxy"] = self.orig_http_proxy
         elif "http_proxy" in os.environ:
             del os.environ["http_proxy"]
+
+        if self.orig_https_proxy:
+            os.environ["https_proxy"] = self.orig_https_proxy
+        elif "https_proxy" in os.environ:
+            del os.environ["https_proxy"]
 
         libcloud.common.base.ALLOW_PATH_DOUBLE_SLASHES = False
 
