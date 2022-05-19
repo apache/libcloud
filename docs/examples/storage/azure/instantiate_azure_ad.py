8a6055eac0a291ae4818f148f0027efaca27f2d7
Brooke White <brooke.white@blackrock.com>
2022-05-19 09:15:23 -0400

    black reformatting

--- docs/examples/storage/azure/instantiate_azure_ad.py
+++ docs/examples/storage/azure/instantiate_azure_ad.py
@@ -3,8 +3,10 @@ from build.lib.libcloud.storage.types import Provider
 
 cls = get_driver(Provider.AZURE_BLOBS)
 
-driver = cls(key="your storage account name",
-             secret="your service principal secret key",
-             tenant_id="your tenant id",
-             identity="your service principal application id",
-             auth_type="azureAd")
+driver = cls(
+    key="your storage account name",
+    secret="your service principal secret key",
+    tenant_id="your tenant id",
+    identity="your service principal application id",
+    auth_type="azureAd",
+)
