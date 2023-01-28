f43cad9be7c59c52b3b4a8c494c7da01a395b2da
Tomaz Muraus <tomaz@tomaz.me>
2023-01-28 20:29:40 +0100

    Fix lint

--- libcloud/loadbalancer/drivers/__init__.py
+++ libcloud/loadbalancer/drivers/__init__.py
@@ -13,7 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__all__ = [
-    "rackspace",
-    "nttcis"
-]
+__all__ = ["rackspace", "nttcis"]
