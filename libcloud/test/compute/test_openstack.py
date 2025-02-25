43d11604511c05ec77aeb0bd17e3438006944452
Miguel Caballer <micafer1@upv.es>
2025-02-25 13:33:55 +0100

    Remove test

--- libcloud/test/compute/test_openstack.py
+++ libcloud/test/compute/test_openstack.py
@@ -2552,16 +2552,6 @@ class OpenStack_2_Tests(OpenStack_1_1_Tests):
         ip = OpenStack_1_1_FloatingIpAddress("foo-bar-id", "42.42.42.42", None)
         self.assertTrue(self.driver.ex_delete_floating_ip(ip))
 
-    def test_ex_attach_floating_ip_to_node(self):
-        image = NodeImage(id=11, name="Ubuntu 8.10 (intrepid)", driver=self.driver)
-        size = NodeSize(1, "256 slice", None, None, None, None, driver=self.driver)
-        node = self.driver.create_node(name="racktest", image=image, size=size)
-        node.id = 4242
-        ip = "42.42.42.42"
-        port_id = "ce531f90-199f-48c0-816c-13e38010b442"
-
-        self.assertTrue(self.driver.ex_attach_floating_ip_to_node(node, ip, port_id))
-
     def test_ex_list_leases(self):
         leases = self.driver.ex_list_leases()
         self.assertEqual(len(leases), 1)
