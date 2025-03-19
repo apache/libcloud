22dc770d2da4c0e69bed1e4f94da1ff9306955e2
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-03-19 12:03:34 -0600

    Remove unavailable vendors

--- docs/examples/compute/auroracompute/instantiate_driver.py
+++
@@ -1,8 +0,0 @@
-from libcloud.compute.types import Provider
-from libcloud.compute.providers import get_driver
-
-apikey = "mykey"
-secretkey = "mysecret"
-
-Driver = get_driver(Provider.AURORACOMPUTE)
-conn = Driver(key=apikey, secret=secretkey)
